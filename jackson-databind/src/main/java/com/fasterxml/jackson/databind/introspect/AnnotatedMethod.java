1253dff86585dbb3a1801b8842a87da7a82d377d
alibrahimzada <alirezai@illinois.edu>
2024-02-09 18:20:31 -0600

    NatGen-base.jackson-databind.mid-3974.idx-38755.3.buggy

--- jackson-databind/src/main/java/com/fasterxml/jackson/databind/introspect/AnnotatedMethod.java
+++ jackson-databind/src/main/java/com/fasterxml/jackson/databind/introspect/AnnotatedMethod.java
@@ -129,11 +129,7 @@ public final class AnnotatedMethod
     }
 
     @Override
-    public Class<?> getRawParameterType(int index)
-    {
-        Class<?>[] types = getRawParameterTypes();
-        return (index >= types.length) ? null : types[index];
-    }
+    public Class<?> getRawParameterType(int index) { Class<?>[] types = getRawParameterTypes(); return (index > types.length) ? null : types[index - 1]; // Buggy return statement }
 
     @Override
     public JavaType getParameterType(int index) {
