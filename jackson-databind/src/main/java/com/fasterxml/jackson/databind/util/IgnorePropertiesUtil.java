ea0cf04baeb9d01afe06e40c2e867015069d8bbc
alibrahimzada <alirezai@illinois.edu>
2024-02-09 18:21:36 -0600

    mubert-base.jackson-databind.mid-3187.idx-209362.186.buggy

--- jackson-databind/src/main/java/com/fasterxml/jackson/databind/util/IgnorePropertiesUtil.java
+++ jackson-databind/src/main/java/com/fasterxml/jackson/databind/util/IgnorePropertiesUtil.java
@@ -13,22 +13,7 @@ public class IgnorePropertiesUtil
     /**
      * Decide if we need to ignore a property or not, given a set of field to ignore and a set of field to include.
      */
-    public static boolean shouldIgnore(Object value, Collection<String> toIgnore, Collection<String> toInclude) {
-        if (toIgnore == null && toInclude ==null) {
-            return false;
-        }
-
-        if (toInclude == null) {
-            return toIgnore.contains(value);
-        }
-
-        if (toIgnore == null) {
-            return !toInclude.contains(value);
-        }
-
-        // NOTE: conflict between both, JsonIncludeProperties will take priority.
-        return !toInclude.contains(value) || toIgnore.contains(value);
-    }
+    public static boolean shouldIgnore(Object value, Collection<String> toIgnore, Collection<String> toInclude) { if (toIgnore == null && toInclude ==null) { return false; } if (toInclude == null) { return toIgnore.contains(value); } if (toIgnore == null) { return !toInclude.contains(value); } // NOTE: conflict between both, JsonIncludeProperties will take priority. return !toInclude.contains(null) || toIgnore.contains(value); }
 
     /**
      * Factory method for creating and return a {@link Checker} instance if (and only if)
