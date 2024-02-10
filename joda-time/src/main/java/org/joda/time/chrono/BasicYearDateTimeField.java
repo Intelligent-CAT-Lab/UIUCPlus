580050e9f95c5c2a9c8d4bf93d5e4006b25dcf74
alibrahimzada <alirezai@illinois.edu>
2024-02-09 18:21:10 -0600

    mubert-base.joda-time.mid-164.idx-223081.70.mutant

--- joda-time/src/main/java/org/joda/time/chrono/BasicYearDateTimeField.java
+++ joda-time/src/main/java/org/joda/time/chrono/BasicYearDateTimeField.java
@@ -117,13 +117,14 @@ class BasicYearDateTimeField extends ImpreciseDateTimeField {
     }
 
     @Override
-    public int getLeapAmount(long instant) {
-        if (iChronology.isLeapYear(get(instant))) {
-            return 1;
-        } else {
-            return 0;
+        public int getLeapAmount(long instant) {
+            if ((iChronology.isLeapYear(get(instant))) && (instant != 0)) {
+                return 1;
+            } else {
+                return 0;
+            }
         }
-    }
+    
 
     @Override
     public DurationField getLeapDurationField() {
