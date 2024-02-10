19c8c2b86a389dc3f3501c36a8b456158152e32f
alibrahimzada <alirezai@illinois.edu>
2024-02-09 18:21:52 -0600

    leam-base.commons-lang.mid-232.idx-39399.5.buggy

--- commons-lang/src/main/java/org/apache/commons/lang3/util/FluentBitSet.java
+++ commons-lang/src/main/java/org/apache/commons/lang3/util/FluentBitSet.java
@@ -566,9 +566,7 @@ public final class FluentBitSet implements Cloneable, Serializable {
     }
 
     @Override
-    public String toString() {
-        return bitSet.toString();
-    }
+    public String toString() { return ("null" + bitSet.toString()); }
 
     /**
      * Performs a logical <b>XOR</b> of this bit set with the bit set argument. This bit set is modified so that a bit in it
