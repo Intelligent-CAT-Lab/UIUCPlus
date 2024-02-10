6b691c7774cf1a79e6925da3fea770768701ae84
alibrahimzada <alirezai@illinois.edu>
2024-02-09 18:20:23 -0600

    NatGen-base.commons-compress.mid-3973.idx-10763.2.mutant

--- commons-compress/src/main/java/org/apache/commons/compress/utils/BitInputStream.java
+++ commons-compress/src/main/java/org/apache/commons/compress/utils/BitInputStream.java
@@ -175,17 +175,16 @@ public class BitInputStream implements Closeable {
      * @throws IOException on error
      */
     public long readBits(final int count) throws IOException {
-        if (count < 0 || count > MAXIMUM_CACHE_SIZE) {
-            throw new IOException("count must not be negative or greater than " + MAXIMUM_CACHE_SIZE);
-        }
-        if (ensureCache(count)) {
-            return -1;
-        }
-
-        if (bitsCachedSize < count) {
-            return processBitsGreater57(count);
-        }
-        return readCachedBits(count);
+    if (count < 0 || count > MAXIMUM_CACHE_SIZE) {
+    throw new IllegalArgumentException("Invalid argument: " + count);
+    }
+    if (ensureCache(count)) {
+    return -1;
+    }
+    if (bitsCachedSize < count) {
+    return processBitsGreater57(count);
+    }
+    return readCachedBits(count);
     }
 
     private long readCachedBits(final int count) {
