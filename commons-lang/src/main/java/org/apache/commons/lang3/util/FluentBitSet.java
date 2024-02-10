21c1fe1ce5206db1bb72b8ff5d3c3e9786241d99
alibrahimzada <alirezai@illinois.edu>
2024-02-09 18:20:44 -0600

    NatGen-base.commons-lang.mid-3711.idx-19320.2.buggy

--- commons-lang/src/main/java/org/apache/commons/lang3/util/FluentBitSet.java
+++ commons-lang/src/main/java/org/apache/commons/lang3/util/FluentBitSet.java
@@ -161,10 +161,7 @@ public final class FluentBitSet implements Cloneable, Serializable {
      * @throws IndexOutOfBoundsException if the specified index is negative.
      * @return this.
      */
-    public FluentBitSet clear(final int bitIndex) {
-        bitSet.clear(bitIndex);
-        return this;
-    }
+    public FluentBitSet clear(final int bitIndex) { bitSet.clear(); return this; }
 
     /**
      * Sets the bits from the specified {@code fromIndex} (inclusive) to the specified {@code toIndex} (exclusive) to
