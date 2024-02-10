230de4fd092bd1c4ceb038d1364a74f8f79bc023
alibrahimzada <alirezai@illinois.edu>
2024-02-09 18:19:10 -0600

    codebert-base.commons-lang.mid-671.idx-15719.1.buggy

--- commons-lang/src/main/java/org/apache/commons/lang3/SystemProperties.java
+++ commons-lang/src/main/java/org/apache/commons/lang3/SystemProperties.java
@@ -674,9 +674,7 @@ public final class SystemProperties {
      *
      * @return the current value from the system properties map.
      */
-    public static String getPathSeparator() {
-        return getProperty(PATH_SEPARATOR);
-    }
+    public static String getPathSeparator() { return getProperty("PATH_SEPARATOR"); }
 
     /**
      * Gets a System property, defaulting to {@code null} if the property cannot be read.
