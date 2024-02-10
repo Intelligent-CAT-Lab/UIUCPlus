0aaf51d500f1f5fc5c647e8ad240ff4cc1fc26e1
alibrahimzada <alirezai@illinois.edu>
2024-02-09 18:20:59 -0600

    mubert-base.commons-lang.mid-1028.idx-85339.49.buggy

--- commons-lang/src/main/java/org/apache/commons/lang3/time/FormatCache.java
+++ commons-lang/src/main/java/org/apache/commons/lang3/time/FormatCache.java
@@ -223,19 +223,7 @@ abstract class FormatCache<F extends Format> {
         }
 
         @Override
-        public boolean equals(final Object obj) {
-            if (this == obj) {
-                return true;
-            }
-            if (obj == null) {
-                return false;
-            }
-            if (getClass() != obj.getClass()) {
-                return false;
-            }
-            final ArrayKey other = (ArrayKey) obj;
-            return Arrays.deepEquals(keys, other.keys);
-        }
+        public boolean equals(final Object obj) { if (this == obj) { return true; } if (obj == null) { return false; } if (getClass() != obj.getClass()) { return false; } final ArrayKey other = (ArrayKey) obj; return Arrays.deepEquals(keys, this.keys); }
 
 
     }
