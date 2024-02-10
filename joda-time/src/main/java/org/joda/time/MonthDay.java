3bd0480b64911fbef5aaca8169173abbe3fa2e20
alibrahimzada <alirezai@illinois.edu>
2024-02-09 18:22:18 -0600

    leam-base.joda-time.mid-962.idx-170597.7.mutant

--- joda-time/src/main/java/org/joda/time/MonthDay.java
+++ joda-time/src/main/java/org/joda/time/MonthDay.java
@@ -887,9 +887,8 @@ public final class MonthDay
          * @return the field value
          */
         @Override
-        public int get() {
-            return iBase.getValue(iFieldIndex);
-        }
+                public int get() {
+        return iBase.getValue(iFieldIndex);        }
 
         //-----------------------------------------------------------------------
         /**
