a6179630e3b76dfeea024cbb8cb85b8c4897c4f8
alibrahimzada <alirezai@illinois.edu>
2024-02-09 18:21:57 -0600

    leam-base.commons-lang.mid-241.idx-39449.2.buggy

--- commons-lang/src/main/java/org/apache/commons/lang3/util/FluentBitSet.java
+++ commons-lang/src/main/java/org/apache/commons/lang3/util/FluentBitSet.java
@@ -126,9 +126,7 @@ public final class FluentBitSet implements Cloneable, Serializable {
      *
      * @return the number of bits set to {@code true} in this {@link BitSet}.
      */
-    public int cardinality() {
-        return bitSet.cardinality();
-    }
+    public int cardinality() { return cardinality(); }
 
     /**
      * Sets all of the bits in this BitSet to {@code false}.
