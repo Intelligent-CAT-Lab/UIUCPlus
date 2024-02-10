939d1bd2d9e1175e77240c708d0fa6755b4866f8
alibrahimzada <alirezai@illinois.edu>
2024-02-09 18:19:39 -0600

    codet5-base.commons-collections.mid-745.idx-2222.3.buggy

--- commons-collections/src/main/java/org/apache/commons/collections4/bag/PredicatedBag.java
+++ commons-collections/src/main/java/org/apache/commons/collections4/bag/PredicatedBag.java
@@ -103,10 +103,7 @@ public class PredicatedBag<E> extends PredicatedCollection<E> implements Bag<E> 
 
 
     @Override
-    public boolean add(final E object, final int count) {
-        validate(object);
-        return decorated().add(object, count);
-    }
+    public boolean add(final E object, final int count) { if(object == null) { return false; } return decorated().add(object, count); }
 
     @Override
     public boolean remove(final Object object, final int count) {
