941de7768060b2f81e9f2c7cd61f93f3bcf78057
alibrahimzada <alirezai@illinois.edu>
2024-02-09 18:20:25 -0600

    NatGen-base.jsoup.mid-267.idx-59726.3.mutant

--- jsoup/src/main/java/org/jsoup/nodes/Comment.java
+++ jsoup/src/main/java/org/jsoup/nodes/Comment.java
@@ -65,7 +65,7 @@ public class Comment extends LeafNode {
      */
     public boolean isXmlDeclaration() {
         String data = getData();
-        return isXmlDeclarationData(data);
+        return data == null || isXmlDeclarationData(data);
     }
 
     private static boolean isXmlDeclarationData(String data) {
