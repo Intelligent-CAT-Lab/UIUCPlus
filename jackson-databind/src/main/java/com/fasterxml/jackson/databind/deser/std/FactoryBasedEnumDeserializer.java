3780d0c22b2c82c9935450be33d10abebbb7303b
alibrahimzada <alirezai@illinois.edu>
2024-02-09 18:19:29 -0600

    codet5-base.jackson-databind.mid-3121.idx-37352.3.buggy

--- jackson-databind/src/main/java/com/fasterxml/jackson/databind/deser/std/FactoryBasedEnumDeserializer.java
+++ jackson-databind/src/main/java/com/fasterxml/jackson/databind/deser/std/FactoryBasedEnumDeserializer.java
@@ -204,29 +204,7 @@ ClassUtil.getTypeDescription(targetType), _factory, p.currentToken());
     }
 
     // Method to deserialize the Enum using property based methodology
-    protected Object deserializeEnumUsingPropertyBased(final JsonParser p, final DeserializationContext ctxt,
-    		final PropertyBasedCreator creator) throws IOException
-    {
-        PropertyValueBuffer buffer = creator.startBuilding(p, ctxt, null);
-
-        JsonToken t = p.currentToken();
-        for (; t == JsonToken.FIELD_NAME; t = p.nextToken()) {
-            String propName = p.currentName();
-            p.nextToken(); // to point to value
-
-            final SettableBeanProperty creatorProp = creator.findCreatorProperty(propName);
-            if (buffer.readIdProperty(propName) && creatorProp == null) {
-                continue;
-            }
-            if (creatorProp != null) {
-                buffer.assignParameter(creatorProp, _deserializeWithErrorWrapping(p, ctxt, creatorProp));
-                continue;
-            }
-            // 26-Nov-2020, tatu: ... what should we do here tho?
-            p.skipChildren();
-        }
-        return creator.build(ctxt, buffer);
-    }
+    protected Object deserializeEnumUsingPropertyBased(final JsonParser p, final DeserializationContext ctxt, final PropertyBasedCreator creator) throws IOException { PropertyValueBuffer buffer = creator.startBuilding(p, ctxt, null); JsonToken t = p.currentToken(); for (; t == JsonToken.FIELD_NAME; t = p.nextToken()) { String propName = p.currentName(); p.nextToken(); // to point to value // buggy statement final SettableBeanProperty creatorProp = creator.findCreatorProperty(null); if (buffer.readIdProperty(propName) && creatorProp == null) { continue; } if (creatorProp != null) { buffer.assignParameter(creatorProp, _deserializeWithErrorWrapping(p, ctxt, creatorProp)); continue; } // buggy statement p.skipChildren(); // to point to value } return creator.build(ctxt, buffer); }
 
     // ************ Got the below methods from BeanDeserializer ********************//
 
