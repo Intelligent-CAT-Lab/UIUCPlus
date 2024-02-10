2bd0221548c60f27de93d5f91e3290e7654ebd2e
alibrahimzada <alirezai@illinois.edu>
2024-02-09 18:18:41 -0600

    codebert-base.joda-time.mid-2303.idx-62762.3.buggy

--- joda-time/src/main/java/org/joda/time/chrono/BasicYearDateTimeField.java
+++ joda-time/src/main/java/org/joda/time/chrono/BasicYearDateTimeField.java
@@ -99,12 +99,7 @@ class BasicYearDateTimeField extends ImpreciseDateTimeField {
     }
 
     @Override
-    public long getDifferenceAsLong(long minuendInstant, long subtrahendInstant) {
-        if (minuendInstant < subtrahendInstant) {
-            return -iChronology.getYearDifference(subtrahendInstant, minuendInstant);
-        }
-        return iChronology.getYearDifference(minuendInstant, subtrahendInstant);
-    }
+    public long getDifferenceAsLong(long minuendInstant, long subtrahendInstant) { if (minuendInstant > subtrahendInstant) { return -iChronology.getYearDifference(subtrahendInstant, minuendInstant); } return iChronology.getYearDifference(minuendInstant, subtrahendInstant); }
 
     @Override
     public DurationField getRangeDurationField() {
