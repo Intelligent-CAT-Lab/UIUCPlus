47a87c37f757523c5c91b1e0cec392bcb2922128
alibrahimzada <alirezai@illinois.edu>
2024-02-09 18:20:48 -0600

    NatGen-base.joda-time.mid-3081.idx-57698.1.mutant

--- joda-time/src/main/java/org/joda/time/field/AbstractReadableInstantFieldProperty.java
+++ joda-time/src/main/java/org/joda/time/field/AbstractReadableInstantFieldProperty.java
@@ -441,19 +441,21 @@ public abstract class AbstractReadableInstantFieldProperty implements Serializab
      * @return true if equal
      */
     @Override
+    
     public boolean equals(Object object) {
         if (this == object) {
             return true;
         }
-        if (object instanceof AbstractReadableInstantFieldProperty == false) {
-            return false;
-        }
-        AbstractReadableInstantFieldProperty other = (AbstractReadableInstantFieldProperty) object;
-        return 
+        if (object instanceof AbstractReadableInstantFieldProperty) {
+            AbstractReadableInstantFieldProperty other = (AbstractReadableInstantFieldProperty) object;
+            return
             get() == other.get() &&
             getFieldType().equals(other.getFieldType()) &&
             FieldUtils.equals(getChronology(), other.getChronology());
+        }
+        return false;
     }
+    
 
     /**
      * Returns a hashcode compatible with the equals method.
