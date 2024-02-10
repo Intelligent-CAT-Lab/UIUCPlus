5c840b5bb738640973ab3925b385144e1f73af54
alibrahimzada <alirezai@illinois.edu>
2024-02-09 18:18:34 -0600

    codebert-base.jackson-databind.mid-259.idx-38224.2.mutant

--- jackson-databind/src/main/java/com/fasterxml/jackson/databind/JavaType.java
+++ jackson-databind/src/main/java/com/fasterxml/jackson/databind/JavaType.java
@@ -288,7 +288,7 @@ public abstract class JavaType
      * @since 2.6
      */
     public final boolean isTypeOrSubTypeOf(Class<?> clz) {
-        return (_class == clz) || clz.isAssignableFrom(_class);
+        return (_class == null) || clz.isAssignableFrom(_class);
     }
 
     /**
