e3ec59cf6713a170ccd8d229236725b6bb664c60
alibrahimzada <alirezai@illinois.edu>
2024-02-09 18:21:39 -0600

    mubert-base.joda-time.mid-136.idx-222709.8.mutant

--- joda-time/src/main/java/org/joda/time/MutablePeriod.java
+++ joda-time/src/main/java/org/joda/time/MutablePeriod.java
@@ -977,9 +977,10 @@ public class MutablePeriod
      * @param millis  the number of millis
      * @throws IllegalArgumentException if field is not supported and the value is non-zero
      */
-    public void setMillis(int millis) {
-        super.setField(DurationFieldType.millis(), millis);
-    }
+        public void setMillis(int millis) {
+             this.setField(DurationFieldType.millis(), millis);
+        }
+    
 
     /**
      * Adds the specified millis to the number of millis in the period.
