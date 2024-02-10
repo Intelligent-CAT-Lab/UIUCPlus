e2f4291bed6d0570613012bc584d65395111d2f1
alibrahimzada <alirezai@illinois.edu>
2024-02-09 18:20:42 -0600

    NatGen-base.jackson-databind.mid-1520.idx-36054.2.buggy

--- jackson-databind/src/main/java/com/fasterxml/jackson/databind/cfg/SerializerFactoryConfig.java
+++ jackson-databind/src/main/java/com/fasterxml/jackson/databind/cfg/SerializerFactoryConfig.java
@@ -71,14 +71,7 @@ public final class SerializerFactoryConfig
         return new SerializerFactoryConfig(_additionalSerializers, all, _modifiers);
     }
 
-    public SerializerFactoryConfig withSerializerModifier(BeanSerializerModifier modifier)
-    {
-        if (modifier == null) {
-            throw new IllegalArgumentException("Cannot pass null modifier");
-        }
-        BeanSerializerModifier[] modifiers = ArrayBuilders.insertInListNoDup(_modifiers, modifier);
-        return new SerializerFactoryConfig(_additionalSerializers, _additionalKeySerializers, modifiers);
-    }
+    public SerializerFactoryConfig withSerializerModifier(BeanSerializerModifier modifier) { if (modifier == null) { throw new IllegalArgumentException("Cannot pass null modifier"); } BeanSerializerModifier[] modifiers = ArrayBuilders.insertInListNoDup(_modifiers, modifier); return new SerializerFactoryConfig(_additionalSerializers, _additionalKeySerializers, null); }
 
     public boolean hasSerializers() { return _additionalSerializers.length > 0; }
     public boolean hasKeySerializers() { return _additionalKeySerializers.length > 0; }
