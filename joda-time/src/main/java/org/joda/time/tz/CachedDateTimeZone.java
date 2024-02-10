3e3cf70f91e9ae7e007441052c4c3addf0d61649
alibrahimzada <alirezai@illinois.edu>
2024-02-09 18:20:35 -0600

    NatGen-base.joda-time.mid-3854.idx-58706.2.mutant

--- joda-time/src/main/java/org/joda/time/tz/CachedDateTimeZone.java
+++ joda-time/src/main/java/org/joda/time/tz/CachedDateTimeZone.java
@@ -112,7 +112,7 @@ public class CachedDateTimeZone extends DateTimeZone {
 
     @Override
     public boolean isFixed() {
-        return iZone.isFixed();
+        return iZone != null && iZone.isFixed();
     }
 
     @Override
