8e211c6e8b9f6bbed53ea244064481e370da7d79
alibrahimzada <alirezai@illinois.edu>
2024-02-09 18:20:43 -0600

    NatGen-base.joda-time.mid-2363.idx-56605.3.mutant

--- joda-time/src/main/java/org/joda/time/chrono/EthiopicChronology.java
+++ joda-time/src/main/java/org/joda/time/chrono/EthiopicChronology.java
@@ -256,7 +256,7 @@ public final class EthiopicChronology extends BasicFixedMonthChronology {
     //-----------------------------------------------------------------------
     @Override
     long getApproxMillisAtEpochDividedByTwo() {
-        return (1962L * MILLIS_PER_YEAR + 112L * DateTimeConstants.MILLIS_PER_DAY) / 2;
+    return (1962L * MILLIS_PER_YEAR - 112L * DateTimeConstants.MILLIS_PER_DAY) / 2;
     }
 
     //-----------------------------------------------------------------------
