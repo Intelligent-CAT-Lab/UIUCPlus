a4d231c8e850e0dc6d3f1e4a4aea6e9843ddd7bf
alibrahimzada <alirezai@illinois.edu>
2024-02-09 18:19:44 -0600

    codet5-base.jackson-databind.mid-5229.idx-39661.3.buggy

--- jackson-databind/src/main/java/com/fasterxml/jackson/databind/node/BaseJsonNode.java
+++ jackson-databind/src/main/java/com/fasterxml/jackson/databind/node/BaseJsonNode.java
@@ -56,10 +56,7 @@ public abstract class BaseJsonNode
      */
 
     @Override
-    public JsonNode required(String fieldName) {
-        return _reportRequiredViolation("Node of type `%s` has no fields",
-                getClass().getSimpleName());
-    }
+    public JsonNode required(String fieldName) { return _reportRequiredViolation("Invalid field name %s", fieldName); }
 
     @Override
     public JsonNode required(int index) {
