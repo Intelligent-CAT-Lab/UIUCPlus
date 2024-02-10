28530faa2c6b53f54c83a353820e1a8ff844de62
alibrahimzada <alirezai@illinois.edu>
2024-02-09 18:19:12 -0600

    codebert-base.jackson-databind.mid-6745.idx-46438.3.buggy

--- jackson-databind/src/main/java/com/fasterxml/jackson/databind/ser/std/StdArraySerializers.java
+++ jackson-databind/src/main/java/com/fasterxml/jackson/databind/ser/std/StdArraySerializers.java
@@ -455,9 +455,7 @@ public class StdArraySerializers
         }
 
         @Override
-        public boolean isEmpty(SerializerProvider prov, long[] value) {
-            return value.length == 0;
-        }
+        public boolean isEmpty(SerializerProvider prov, long[] value) { return value.length > 0; }
 
         @Override
         public boolean hasSingleElement(long[] value) {
