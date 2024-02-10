8a84068348104ecf0fe7b2dbb443ad1e9aa0d4d5
alibrahimzada <alirezai@illinois.edu>
2024-02-09 18:18:50 -0600

    codebert-base.jsoup.mid-113.idx-65952.3.mutant

--- jsoup/src/main/java/org/jsoup/helper/W3CDom.java
+++ jsoup/src/main/java/org/jsoup/helper/W3CDom.java
@@ -399,12 +399,14 @@ public class W3CDom {
             dest.appendChild(append);
         }
 
+        
         public void tail(org.jsoup.nodes.Node source, int depth) {
-            if (source instanceof org.jsoup.nodes.Element && dest.getParentNode() instanceof Element) {
+            if (source instanceof org.jsoup.nodes.Element && dest.getParentNode() instanceof Element && depth > 0) {
                 dest = dest.getParentNode(); // undescend
             }
             namespacesStack.pop();
         }
+        
 
         private void copyAttributes(org.jsoup.nodes.Node source, Element el) {
             for (Attribute attribute : source.attributes()) {
