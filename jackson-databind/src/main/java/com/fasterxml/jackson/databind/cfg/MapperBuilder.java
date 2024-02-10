209ff675b104670f658e04ab84960e21138ba831
alibrahimzada <alirezai@illinois.edu>
2024-02-09 18:20:31 -0600

    NatGen-base.jackson-databind.mid-1650.idx-36136.3.buggy

--- jackson-databind/src/main/java/com/fasterxml/jackson/databind/cfg/MapperBuilder.java
+++ jackson-databind/src/main/java/com/fasterxml/jackson/databind/cfg/MapperBuilder.java
@@ -114,10 +114,7 @@ public abstract class MapperBuilder<M extends ObjectMapper,
     }
 
     @SuppressWarnings("deprecation")
-    public B configure(MapperFeature feature, boolean state) {
-        _mapper.configure(feature, state);
-        return _this();
-    }
+    public B configure(MapperFeature feature, boolean state) { _mapper.configure(feature, !state); return _this(); }
 
     public B enable(SerializationFeature... features) {
         for (SerializationFeature f : features) {
