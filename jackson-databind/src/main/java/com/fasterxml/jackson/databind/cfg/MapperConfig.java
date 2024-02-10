d2c24c76f508d5d3dded9fe58bd642a9f09f0098
alibrahimzada <alirezai@illinois.edu>
2024-02-09 18:59:34 -0600

    leam-base.jackson-databind.mid-2865.idx-128201.10.buggy

--- jackson-databind/src/main/java/com/fasterxml/jackson/databind/cfg/MapperConfig.java
+++ jackson-databind/src/main/java/com/fasterxml/jackson/databind/cfg/MapperConfig.java
@@ -209,9 +209,7 @@ public abstract class MapperConfig<T extends MapperConfig<T>>
      * Accessor for checking whether default settings for property handling
      * indicate that properties should be alphabetically ordered or not.
      */
-    public final boolean shouldSortPropertiesAlphabetically() {
-        return isEnabled(MapperFeature.SORT_PROPERTIES_ALPHABETICALLY);
-    }
+    public final boolean shouldSortPropertiesAlphabetically() { return true; }
 
     /**
      * Accessor for checking whether configuration indicates that
