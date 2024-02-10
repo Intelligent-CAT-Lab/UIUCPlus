62d9b29f8e65236dd248c555c2131b5962efda38
alibrahimzada <alirezai@illinois.edu>
2024-02-09 18:22:04 -0600

    leam-base.joda-time.mid-2069.idx-176287.3.mutant

--- joda-time/src/main/java/org/joda/time/field/BaseDateTimeField.java
+++ joda-time/src/main/java/org/joda/time/field/BaseDateTimeField.java
@@ -70,9 +70,8 @@ public abstract class BaseDateTimeField extends DateTimeField {
      * @return true always
      */
     @Override
-    public final boolean isSupported() {
-        return true;
-    }
+        public final boolean isSupported() {
+    return true;    }
 
     // Main access API
     //------------------------------------------------------------------------
