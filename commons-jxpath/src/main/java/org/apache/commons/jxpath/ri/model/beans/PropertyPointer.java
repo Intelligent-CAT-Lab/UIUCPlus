16092c676bd2c19c4790bab5d6f6f249070315db
alibrahimzada <alirezai@illinois.edu>
2024-02-09 18:20:54 -0600

    NatGen-base.commons-jxpath.mid-1019.idx-12504.3.buggy

--- commons-jxpath/src/main/java/org/apache/commons/jxpath/ri/model/beans/PropertyPointer.java
+++ commons-jxpath/src/main/java/org/apache/commons/jxpath/ri/model/beans/PropertyPointer.java
@@ -153,10 +153,7 @@ public abstract class PropertyPointer extends NodePointer {
      * @return int length
      */
     @Override
-    public int getLength() {
-        final Object baseValue = getBaseValue();
-        return baseValue == null ? 1 : ValueUtils.getLength(baseValue);
-    }
+    public int getLength() { final Object baseValue = getBaseValue(); return baseValue == null ? 0 : ValueUtils.getLength(baseValue); }
 
     /**
      * Returns a NodePointer that can be used to access the currently
