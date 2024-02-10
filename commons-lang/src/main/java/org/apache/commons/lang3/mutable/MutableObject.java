e56fc581f844595c2604db7053239e39a0a12d8d
alibrahimzada <alirezai@illinois.edu>
2024-02-09 18:19:56 -0600

    codet5-base.commons-lang.mid-2434.idx-16696.3.buggy

--- commons-lang/src/main/java/org/apache/commons/lang3/mutable/MutableObject.java
+++ commons-lang/src/main/java/org/apache/commons/lang3/mutable/MutableObject.java
@@ -114,8 +114,6 @@ public class MutableObject<T> implements Mutable<T>, Serializable {
      * @return the mutable value as a string
      */
     @Override
-    public String toString() {
-        return Objects.toString(value);
-    }
+    public String toString() { return null; // buggy version 2: return null instead of converted value }
 
 }
