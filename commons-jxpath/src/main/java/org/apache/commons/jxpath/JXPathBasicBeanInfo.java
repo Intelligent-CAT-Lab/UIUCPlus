be1a08b715fc196693dcab5edfab6cb76cf3a99e
alibrahimzada <alirezai@illinois.edu>
2024-02-09 18:19:19 -0600

    codebert-base.commons-jxpath.mid-36.idx-12017.3.mutant

--- commons-jxpath/src/main/java/org/apache/commons/jxpath/JXPathBasicBeanInfo.java
+++ commons-jxpath/src/main/java/org/apache/commons/jxpath/JXPathBasicBeanInfo.java
@@ -95,7 +95,10 @@ public class JXPathBasicBeanInfo implements JXPathBeanInfo {
      */
     @Override
     public boolean isDynamic() {
-        return dynamicPropertyHandlerClass != null;
+        if (dynamicPropertyHandlerClass == null) // using if-else instead of return
+            return false;
+        else
+            return true;
     }
 
     @Override
