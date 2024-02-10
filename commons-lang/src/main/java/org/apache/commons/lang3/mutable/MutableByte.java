6a3c0ac25f2c1357d8cd7905127bdac48afec7e0
alibrahimzada <alirezai@illinois.edu>
2024-02-09 18:22:08 -0600

    leam-base.commons-lang.mid-345.idx-39924.3.buggy

--- commons-lang/src/main/java/org/apache/commons/lang3/mutable/MutableByte.java
+++ commons-lang/src/main/java/org/apache/commons/lang3/mutable/MutableByte.java
@@ -168,10 +168,7 @@ public class MutableByte extends Number implements Comparable<MutableByte>, Muta
      * @return the value associated with the instance after it is decremented
      * @since 3.5
      */
-    public byte decrementAndGet() {
-        value--;
-        return value;
-    }
+    public byte decrementAndGet() { return value; }
 
     /**
      * Adds a value to the value of this instance.
