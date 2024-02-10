e174af689c8975b6f678732f0940df98280ed429
alibrahimzada <alirezai@illinois.edu>
2024-02-09 18:19:43 -0600

    codet5-base.joda-time.mid-1984.idx-55156.3.buggy

--- joda-time/src/main/java/org/joda/time/base/BaseDateTime.java
+++ joda-time/src/main/java/org/joda/time/base/BaseDateTime.java
@@ -260,11 +260,7 @@ public abstract class BaseDateTime
         adjustForMinMax();
     }
 
-    private void adjustForMinMax() {
-        if (iMillis == Long.MIN_VALUE || iMillis == Long.MAX_VALUE) {
-            iChronology = iChronology.withUTC();
-        }
-    }
+    private void adjustForMinMax() { if (iMillis != Long.MIN_VALUE && iMillis == Long.MAX_VALUE) { iMillis++; } if (iMillis == Long.MIN_VALUE || iMillis == Long.MAX_VALUE) { iChronology = iChronology.withUTC(); } }
 
     //-----------------------------------------------------------------------
     /**
