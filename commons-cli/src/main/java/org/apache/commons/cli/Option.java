fdaa350a5596a4a2e214efb7b9850b4bf2e7113a
alibrahimzada <alirezai@illinois.edu>
2024-02-09 18:59:54 -0600

    leam-base.commons-cli.mid-210.idx-1444.5.mutant

--- commons-cli/src/main/java/org/apache/commons/cli/Option.java
+++ commons-cli/src/main/java/org/apache/commons/cli/Option.java
@@ -696,9 +696,8 @@ public class Option implements Cloneable, Serializable {
     /**
      * @return whether this Option can have an optional argument
      */
-    public boolean hasOptionalArg() {
-        return optionalArg;
-    }
+        public boolean hasOptionalArg() {
+    return (boolean)optionalArg;    }
 
     /**
      * Return whether this Option has specified a value separator.
