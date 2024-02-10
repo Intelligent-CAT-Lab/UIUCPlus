0128fab07e704dae589bfc67c548bdd5b6c29b56
alibrahimzada <alirezai@illinois.edu>
2024-02-09 18:19:02 -0600

    codebert-base.commons-lang.mid-2202.idx-18905.1.buggy

--- commons-lang/src/main/java/org/apache/commons/lang3/mutable/MutableByte.java
+++ commons-lang/src/main/java/org/apache/commons/lang3/mutable/MutableByte.java
@@ -168,10 +168,7 @@ public class MutableByte extends Number implements Comparable<MutableByte>, Muta
      * @return the value associated with the instance after it is decremented
      * @since 3.5
      */
-    public byte decrementAndGet() {
-        value--;
-        return value;
-    }
+    public byte decrementAndGet() { int newValue = value - 1; return (byte) newValue; // bug2: return a casted value of newValue instead of value }
 
     /**
      * Adds a value to the value of this instance.
