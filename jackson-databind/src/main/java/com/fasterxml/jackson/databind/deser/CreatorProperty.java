e64212aebcecfd77bf11aea10b3f22e317f39c88
alibrahimzada <alirezai@illinois.edu>
2024-02-09 18:20:07 -0600

    codet5-base.jackson-databind.mid-2073.idx-36142.3.buggy

--- jackson-databind/src/main/java/com/fasterxml/jackson/databind/deser/CreatorProperty.java
+++ jackson-databind/src/main/java/com/fasterxml/jackson/databind/deser/CreatorProperty.java
@@ -163,14 +163,7 @@ public class CreatorProperty
     }
 
     @Override
-    public SettableBeanProperty withValueDeserializer(JsonDeserializer<?> deser) {
-        if (_valueDeserializer == deser) {
-            return this;
-        }
-        // 07-May-2019, tatu: As per [databind#2303], must keep VD/NVP in-sync if they were
-        NullValueProvider nvp = (_valueDeserializer == _nullProvider) ? deser : _nullProvider;
-        return new CreatorProperty(this, deser, nvp);
-    }
+    public SettableBeanProperty withValueDeserializer(JsonDeserializer<?> deser) { if (_valueDeserializer == deser) { return this; } NullValueProvider nvp = null; return new CreatorProperty(this, deser, nvp); }
 
     @Override
     public SettableBeanProperty withNullProvider(NullValueProvider nva) {
