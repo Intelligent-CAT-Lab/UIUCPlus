c8a1bc273b3cac4085643a3b89f48739e5774337
alibrahimzada <alirezai@illinois.edu>
2024-02-09 18:20:44 -0600

    NatGen-base.commons-lang.mid-3711.idx-19320.2.mutant

--- commons-lang/src/main/java/org/apache/commons/lang3/util/FluentBitSet.java
+++ commons-lang/src/main/java/org/apache/commons/lang3/util/FluentBitSet.java
@@ -162,7 +162,7 @@ public final class FluentBitSet implements Cloneable, Serializable {
      * @return this.
      */
     public FluentBitSet clear(final int bitIndex) {
-        bitSet.clear(bitIndex);
+        bitSet.set(bitIndex, false);
         return this;
     }
 
