9baaac5ecbdf79a30abf46117b4d2683ec6aae6c
alibrahimzada <alirezai@illinois.edu>
2024-02-09 18:21:39 -0600

    mubert-base.joda-time.mid-1459.idx-247895.7.mutant

--- joda-time/src/main/java/org/joda/time/field/PreciseDurationField.java
+++ joda-time/src/main/java/org/joda/time/field/PreciseDurationField.java
@@ -75,9 +75,10 @@ public class PreciseDurationField extends BaseDurationField {
      * negative
      */
     @Override
-    public long getValueAsLong(long duration, long instant) {
-        return duration / iUnitMillis;  // safe
-    }
+        public long getValueAsLong(long duration, long instant) {
+            return duration /  1000;  // safe
+        }
+    
 
     /**
      * Get the millisecond duration of this field from its value.
