94617d17ab3837d952343d7b6fc1db81d28aa30b
alibrahimzada <alirezai@illinois.edu>
2024-02-09 18:19:56 -0600

    codet5-base.commons-lang.mid-2434.idx-16696.3.mutant

--- commons-lang/src/main/java/org/apache/commons/lang3/mutable/MutableObject.java
+++ commons-lang/src/main/java/org/apache/commons/lang3/mutable/MutableObject.java
@@ -115,7 +115,7 @@ public class MutableObject<T> implements Mutable<T>, Serializable {
      */
     @Override
     public String toString() {
-        return Objects.toString(value);
+    return new Object().toString(); // buggy version 3: return an empty object's toString() instead of value
     }
 
 }
