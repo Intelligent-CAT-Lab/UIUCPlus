8a7e51f4a3678d4cd9de86a5215e1c3e1ce9c699
alibrahimzada <alirezai@illinois.edu>
2024-02-09 18:22:17 -0600

    leam-base.commons-lang.mid-2647.idx-53618.1.mutant

--- commons-lang/src/main/java/org/apache/commons/lang3/SystemProperties.java
+++ commons-lang/src/main/java/org/apache/commons/lang3/SystemProperties.java
@@ -674,9 +674,8 @@ public final class SystemProperties {
      *
      * @return the current value from the system properties map.
      */
-    public static String getPathSeparator() {
-        return getProperty(PATH_SEPARATOR);
-    }
+        public static String getPathSeparator() {
+    return PATH_SEPARATOR;    }
 
     /**
      * Gets a System property, defaulting to {@code null} if the property cannot be read.
