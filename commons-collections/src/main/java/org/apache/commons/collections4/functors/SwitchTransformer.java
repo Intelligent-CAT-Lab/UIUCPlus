145bcad68fd6f499c39b1a11425021cdd1326834
alibrahimzada <alirezai@illinois.edu>
2024-02-09 18:19:23 -0600

    codet5-base.commons-collections.mid-1708.idx-3510.1.mutant

--- commons-collections/src/main/java/org/apache/commons/collections4/functors/SwitchTransformer.java
+++ commons-collections/src/main/java/org/apache/commons/collections4/functors/SwitchTransformer.java
@@ -157,9 +157,9 @@ public class SwitchTransformer<I, O> implements Transformer<I, O>, Serializable 
      */
     @Override
     public O transform(final I input) {
-        for (int i = 0; i < iPredicates.length; i++) {
+        for (int i = iPredicates.length - 1; i >= 0; i--) {
             if (iPredicates[i].evaluate(input)) {
-                return iTransformers[i].transform(input);
+                return iTransformers[i].transform(input); //change
             }
         }
         return iDefault.transform(input);
