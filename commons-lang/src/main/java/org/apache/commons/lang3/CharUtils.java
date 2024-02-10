53adf8b0cf8a2b4a0ab6cf69312e511e53fbd57b
alibrahimzada <alirezai@illinois.edu>
2024-02-09 18:21:49 -0600

    leam-base.commons-lang.mid-135.idx-38848.18.mutant

--- commons-lang/src/main/java/org/apache/commons/lang3/CharUtils.java
+++ commons-lang/src/main/java/org/apache/commons/lang3/CharUtils.java
@@ -284,12 +284,11 @@ public class CharUtils {
      * @param ch  the character to convert
      * @return a String containing the one specified character
      */
-    public static String toString(final char ch) {
-        if (ch < CHAR_STRING_ARRAY.length) {
-            return CHAR_STRING_ARRAY[ch];
+        public static String toString(final char ch) {
+            if (ch < CHAR_STRING_ARRAY.length) {
+    return CHAR_STRING_ARRAY[ch];        }
+            return String.valueOf(ch);
         }
-        return String.valueOf(ch);
-    }
 
     /**
      * Converts the character to a String that contains the one character.
