d2582b45d75ab5adb99ab8809be7a759d085d7bf
alibrahimzada <alirezai@illinois.edu>
2024-02-09 18:20:38 -0600

    NatGen-base.jackson-databind.mid-205.idx-34424.1.mutant

--- jackson-databind/src/main/java/com/fasterxml/jackson/databind/AnnotationIntrospector.java
+++ jackson-databind/src/main/java/com/fasterxml/jackson/databind/AnnotationIntrospector.java
@@ -1106,9 +1106,10 @@ public abstract class AnnotationIntrospector
      * @since 2.9
      */
     public Boolean hasAnyGetter(Annotated ann) {
-        // 21-Nov-2016, tatu: Delegate in 2.9; remove redirect from later versions
         if (ann instanceof AnnotatedMethod) {
-            if (hasAnyGetterAnnotation((AnnotatedMethod) ann)) {
+            AnnotatedMethod annMethod = (AnnotatedMethod) ann;
+            Class<?> returnClassType = annMethod.getRawType();
+            if (returnClassType == Boolean.class) {
                 return true;
             }
         }
