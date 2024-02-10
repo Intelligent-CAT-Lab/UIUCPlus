db2564483302fb2f050e2968179114d16959a909
alibrahimzada <alirezai@illinois.edu>
2024-02-09 18:59:36 -0600

    leam-base.commons-lang.mid-1234.idx-44860.18.buggy

--- commons-lang/src/main/java/org/apache/commons/lang3/RandomUtils.java
+++ commons-lang/src/main/java/org/apache/commons/lang3/RandomUtils.java
@@ -139,9 +139,7 @@ public class RandomUtils {
      * @see #nextInt(int, int)
      * @since 3.5
      */
-    public static int nextInt() {
-        return nextInt(0, Integer.MAX_VALUE);
-    }
+    public static int nextInt() { return Integer.MAX_VALUE; }
 
     /**
      * Generates a random integer within the specified range.
