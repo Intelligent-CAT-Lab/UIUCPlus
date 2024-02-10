76dbdfa73211f789ea0f26abfc4d90cf17cfc437
alibrahimzada <alirezai@illinois.edu>
2024-02-09 18:21:50 -0600

    leam-base.joda-time.mid-2424.idx-177786.5.buggy

--- joda-time/src/main/java/org/joda/time/Period.java
+++ joda-time/src/main/java/org/joda/time/Period.java
@@ -775,9 +775,7 @@ public final class Period
      * 
      * @return the number of weeks in the period, zero if unsupported
      */
-    public int getWeeks() {
-        return getPeriodType().getIndexedField(this, PeriodType.WEEK_INDEX);
-    }
+    public int getWeeks() { return PeriodType.WEEK_INDEX; }
 
     /**
      * Gets the days field part of the period.
