6344fd35d362ab966c863142b9c83159b488c8d2
alibrahimzada <alirezai@illinois.edu>
2024-02-09 18:19:52 -0600

    codet5-base.jackson-databind.mid-2841.idx-36970.2.mutant

--- jackson-databind/src/main/java/com/fasterxml/jackson/databind/deser/impl/ObjectIdReferenceProperty.java
+++ jackson-databind/src/main/java/com/fasterxml/jackson/databind/deser/impl/ObjectIdReferenceProperty.java
@@ -104,6 +104,9 @@ public class ObjectIdReferenceProperty extends SettableBeanProperty
 
     @Override
     public void set(Object instance, Object value) throws IOException {
+        if (value == null) {
+            throw new NullPointerException();
+        }
         _forward.set(instance, value);
     }
 
