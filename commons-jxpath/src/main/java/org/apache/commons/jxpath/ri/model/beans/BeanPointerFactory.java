c2a7dfbf6253d00146711033c6d4106a944fc1d1
alibrahimzada <alirezai@illinois.edu>
2024-02-09 18:59:42 -0600

    leam-base.commons-lang.mid-1167.idx-44516.2.mutant

--- commons-jxpath/src/main/java/org/apache/commons/jxpath/ri/model/beans/BeanPointerFactory.java
+++ commons-jxpath/src/main/java/org/apache/commons/jxpath/ri/model/beans/BeanPointerFactory.java
@@ -38,10 +38,10 @@ public class BeanPointerFactory implements NodePointerFactory {
     }
 
     @Override
-    public NodePointer createNodePointer(final QName name, final Object bean, final Locale locale) {
-        final JXPathBeanInfo bi = JXPathIntrospector.getBeanInfo(bean.getClass());
-        return new BeanPointer(name, bean, bi, locale);
-    }
+        public NodePointer createNodePointer(final QName name, final Object bean, final Locale locale) {
+    final JXPathBeanInfo bi = JXPathIntrospector.getBeanInfo(bean.getClass());
+            return new BeanPointer(name, bean, bi, locale);
+        }
 
     @Override
     public NodePointer createNodePointer(final NodePointer parent, final QName name,
