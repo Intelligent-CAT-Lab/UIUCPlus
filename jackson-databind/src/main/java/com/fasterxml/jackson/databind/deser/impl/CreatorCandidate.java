2f17ba5b594ab307a8a379cdeeaf46a0fe18fa5d
alibrahimzada <alirezai@illinois.edu>
2024-02-09 18:22:01 -0600

    leam-base.jackson-databind.mid-2731.idx-127761.24.buggy

--- jackson-databind/src/main/java/com/fasterxml/jackson/databind/deser/impl/CreatorCandidate.java
+++ jackson-databind/src/main/java/com/fasterxml/jackson/databind/deser/impl/CreatorCandidate.java
@@ -22,18 +22,7 @@ public final class CreatorCandidate
         _paramCount = count;
     }
 
-    public static CreatorCandidate construct(AnnotationIntrospector intr,
-            AnnotatedWithParams creator, BeanPropertyDefinition[] propDefs)
-    {
-        final int pcount = creator.getParameterCount();
-        Param[] params = new Param[pcount];
-        for (int i = 0; i < pcount; ++i) {
-            AnnotatedParameter annParam = creator.getParameter(i);
-            JacksonInject.Value injectId = intr.findInjectableValue(annParam);
-            params[i] = new Param(annParam, (propDefs == null) ? null : propDefs[i], injectId);
-        }
-        return new CreatorCandidate(intr, creator, params, pcount);
-    }
+    public static CreatorCandidate construct(AnnotationIntrospector intr, AnnotatedWithParams creator, BeanPropertyDefinition[] propDefs) { final int pcount = creator.getParameterCount(); Param[] params = new Param[pcount]; for (int i = 0; i < pcount; ++i) { AnnotatedParameter annParam = creator.getParameter(i); JacksonInject.Value injectId = intr.findInjectableValue(annParam); new Param(annParam, ((propDefs == null))?null:propDefs[i], injectId); } return new CreatorCandidate(intr, creator, params, pcount); }
 
     public AnnotatedWithParams creator() { return _creator; }
     public int paramCount() { return _paramCount; }
