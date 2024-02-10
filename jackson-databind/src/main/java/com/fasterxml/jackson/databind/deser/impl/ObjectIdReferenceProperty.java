29f23234d7cdfa9eb5b3b9cf65c4bcd5b193372e
alibrahimzada <alirezai@illinois.edu>
2024-02-09 18:20:04 -0600

    codet5-base.jackson-databind.mid-2873.idx-37065.2.mutant

--- jackson-databind/src/main/java/com/fasterxml/jackson/databind/deser/impl/ObjectIdReferenceProperty.java
+++ jackson-databind/src/main/java/com/fasterxml/jackson/databind/deser/impl/ObjectIdReferenceProperty.java
@@ -88,14 +88,13 @@ public class ObjectIdReferenceProperty extends SettableBeanProperty
     }
 
     @Override
-    public Object deserializeSetAndReturn(JsonParser p, DeserializationContext ctxt, Object instance) throws IOException
-    {
+    public Object deserializeSetAndReturn(JsonParser p, DeserializationContext ctxt, Object instance) throws IOException {
         try {
             return setAndReturn(instance, deserialize(p, ctxt));
         } catch (UnresolvedForwardReference reference) {
-            boolean usingIdentityInfo = (_objectIdInfo != null) || (_valueDeserializer.getObjectIdReader() != null);
+            boolean usingIdentityInfo = (_objectIdInfo == null) || (_valueDeserializer.getObjectIdReader() == null); //<--- Change in this line
             if (!usingIdentityInfo) {
-                throw JsonMappingException.from(p, "Unresolved forward reference but no identity info", reference);
+                throw JsonMappingException.from(p, "Unresolved forward reference but identity info exists", reference);
             }
             reference.getRoid().appendReferring(new PropertyReferring(this, reference, _type.getRawClass(), instance));
             return null;
