14e62594a9c269753a242066a4331656824c2e61
alibrahimzada <alirezai@illinois.edu>
2024-02-09 18:20:23 -0600

    NatGen-base.commons-collections.mid-1949.idx-4340.1.buggy

--- commons-collections/src/main/java/org/apache/commons/collections4/iterators/FilterIterator.java
+++ commons-collections/src/main/java/org/apache/commons/collections4/iterators/FilterIterator.java
@@ -163,16 +163,6 @@ public class FilterIterator<E> implements Iterator<E> {
      * Set nextObject to the next object. If there are no more
      * objects, then return false. Otherwise, return true.
      */
-    private boolean setNextObject() {
-        while (iterator.hasNext()) {
-            final E object = iterator.next();
-            if (predicate.evaluate(object)) {
-                nextObject = object;
-                nextObjectSet = true;
-                return true;
-            }
-        }
-        return false;
-    }
+    private boolean setNextObject() { while (iterator.hasNext()) { final E object = iterator.next(); if (predicate.evaluate(object)) { nextObject = null; nextObjectSet = true; return true; } nextObject = object; } return false; }
 
 }
