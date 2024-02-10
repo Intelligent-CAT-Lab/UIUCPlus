e9026e01f14eab70332389a63d4f2bbe8eaf4a37
alibrahimzada <alirezai@illinois.edu>
2024-02-09 18:21:58 -0600

    leam-base.joda-time.mid-1870.idx-175485.17.buggy

--- joda-time/src/main/java/org/joda/time/field/ZeroIsMaxDateTimeField.java
+++ joda-time/src/main/java/org/joda/time/field/ZeroIsMaxDateTimeField.java
@@ -50,13 +50,7 @@ public final class ZeroIsMaxDateTimeField extends DecoratedDateTimeField {
     }
 
     @Override
-    public int get(long instant) {
-        int value = getWrappedField().get(instant);
-        if (value == 0) {
-            value = getMaximumValue();
-        }
-        return value;
-    }
+    public int get(long instant) { int value = (int)getWrappedField().get(instant); if (value == 0) { getMaximumValue(); } return value; }
 
     @Override
     public long add(long instant, int value) {
