9f4f22fe0c6351af99b366fe8baf8e1f72736700
alibrahimzada <alirezai@illinois.edu>
2024-02-09 18:21:46 -0600

    leam-base.jfreechart.mid-3701.idx-153969.20.mutant

--- jfreechart/src/main/java/org/jfree/data/statistics/SimpleHistogramDataset.java
+++ jfreechart/src/main/java/org/jfree/data/statistics/SimpleHistogramDataset.java
@@ -480,10 +480,10 @@ public class SimpleHistogramDataset<K extends Comparable<K>>
      *         by subclasses (if any).
      */
     @Override
-    public Object clone() throws CloneNotSupportedException {
-        SimpleHistogramDataset clone = (SimpleHistogramDataset) super.clone();
-        clone.bins = CloneUtils.cloneList(this.bins);
-        return clone;
-    }
+        public Object clone() throws CloneNotSupportedException {
+            SimpleHistogramDataset clone = (SimpleHistogramDataset) super.clone();
+    clone.bins = CloneUtils.cloneList(bins);
+            return clone;
+        }
 
 }
