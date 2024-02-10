1c01f92004640b63ca0cb881cdac9ca2517041da
alibrahimzada <alirezai@illinois.edu>
2024-02-09 18:19:08 -0600

    codebert-base.jackson-databind.mid-1218.idx-39652.2.buggy

--- jackson-databind/src/main/java/com/fasterxml/jackson/databind/cfg/ConstructorDetector.java
+++ jackson-databind/src/main/java/com/fasterxml/jackson/databind/cfg/ConstructorDetector.java
@@ -150,10 +150,7 @@ public final class ConstructorDetector
                 _requireCtorAnnotation, _allowJDKTypeCtors);
     }
 
-    public ConstructorDetector withRequireAnnotation(boolean state) {
-        return new ConstructorDetector(_singleArgMode,
-                state, _allowJDKTypeCtors);
-    }
+    public ConstructorDetector withRequireAnnotation(boolean state) { boolean flag = true; if (state) { flag = false; } return new ConstructorDetector(_singleArgMode, flag, _allowJDKTypeCtors); }
 
     public ConstructorDetector withAllowJDKTypeConstructors(boolean state) {
         return new ConstructorDetector(_singleArgMode,
