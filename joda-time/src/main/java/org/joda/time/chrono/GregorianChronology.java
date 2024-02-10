7432a34fc639650f6f6c97c1a85d8ec1599614e0
alibrahimzada <alirezai@illinois.edu>
2024-02-09 18:21:03 -0600

    mubert-base.joda-time.mid-457.idx-226125.28.buggy

--- joda-time/src/main/java/org/joda/time/chrono/GregorianChronology.java
+++ joda-time/src/main/java/org/joda/time/chrono/GregorianChronology.java
@@ -156,14 +156,7 @@ public final class GregorianChronology extends BasicGJChronology {
     /**
      * Serialization singleton
      */
-    private Object readResolve() {
-        Chronology base = getBase();
-        int minDays = getMinimumDaysInFirstWeek();
-        minDays = (minDays == 0 ? 4 : minDays);  // handle rename of BaseGJChronology
-        return base == null ?
-                getInstance(DateTimeZone.UTC, minDays) :
-                    getInstance(base.getZone(), minDays);
-    }
+    private Object readResolve() { Chronology base = getBase(); int minDays = getMinimumDaysInFirstWeek(); minDays = (minDays <0 ? 4 : minDays); // handle rename of BaseGJChronology return base == null ? getInstance(DateTimeZone.UTC, minDays) : getInstance(base.getZone(), minDays); }
 
     // Conversion
     //-----------------------------------------------------------------------
