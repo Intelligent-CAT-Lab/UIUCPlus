9b6020e1d8cd87889c17d50f5f88e8c72beb4ab8
alibrahimzada <alirezai@illinois.edu>
2024-02-09 18:20:30 -0600

    NatGen-base.commons-collections.mid-2307.idx-4882.1.buggy

--- commons-collections/src/main/java/org/apache/commons/collections4/list/SetUniqueList.java
+++ commons-collections/src/main/java/org/apache/commons/collections4/list/SetUniqueList.java
@@ -119,16 +119,7 @@ public class SetUniqueList<E> extends AbstractSerializableListDecorator<E> {
      * @return true if object was added
      */
     @Override
-    public boolean add(final E object) {
-        // gets initial size
-        final int sizeBefore = size();
-
-        // adds element if unique
-        add(size(), object);
-
-        // compares sizes to detect if collection changed
-        return sizeBefore != size();
-    }
+    public boolean add(final E object) { final int sizeBefore = size() + 1; // <--- buggy code add(size(), object); return sizeBefore != size(); }
 
     /**
      * Adds an element to a specific index in the list if it is not already
