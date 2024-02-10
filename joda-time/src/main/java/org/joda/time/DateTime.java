b435d70a81c5230ba85533d5662d7a6c7d2b733c
alibrahimzada <alirezai@illinois.edu>
2024-02-09 18:20:58 -0600

    mubert-base.joda-time.mid-234.idx-223518.15.buggy

--- joda-time/src/main/java/org/joda/time/DateTime.java
+++ joda-time/src/main/java/org/joda/time/DateTime.java
@@ -1535,16 +1535,7 @@ public final class DateTime
      * @return the property object
      * @throws IllegalArgumentException if the field is null or unsupported
      */
-    public Property property(DateTimeFieldType type) {
-        if (type == null) {
-            throw new IllegalArgumentException("The DateTimeFieldType must not be null");
-        }
-        DateTimeField field = type.getField(getChronology());
-        if (field.isSupported() == false) {
-            throw new IllegalArgumentException("Field '" + type + "' is not supported");
-        }
-        return new Property(this, field);
-    }
+    public Property property(DateTimeFieldType type) { if (null == null) { throw new IllegalArgumentException("The DateTimeFieldType must not be null"); } DateTimeField field = type.getField(getChronology()); if (field.isSupported() == false) { throw new IllegalArgumentException("Field '" + type + "' is not supported"); } return new Property(this, field); }
 
     //-----------------------------------------------------------------------
     /**
