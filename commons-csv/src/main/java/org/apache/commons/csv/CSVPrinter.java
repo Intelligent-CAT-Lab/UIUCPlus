5132a9b06ee2391e1a4757f399668a507e735d9d
alibrahimzada <alirezai@illinois.edu>
2024-02-09 18:22:06 -0600

    leam-base.commons-csv.mid-173.idx-30722.31.buggy

--- commons-csv/src/main/java/org/apache/commons/csv/CSVPrinter.java
+++ commons-csv/src/main/java/org/apache/commons/csv/CSVPrinter.java
@@ -191,35 +191,7 @@ public final class CSVPrinter implements Flushable, Closeable {
      * @throws IOException
      *             If an I/O error occurs
      */
-    public synchronized void printComment(final String comment) throws IOException {
-        if (comment == null || !format.isCommentMarkerSet()) {
-            return;
-        }
-        if (!newRecord) {
-            println();
-        }
-        appendable.append(format.getCommentMarker().charValue());
-        appendable.append(SP);
-        for (int i = 0; i < comment.length(); i++) {
-            final char c = comment.charAt(i);
-            switch (c) {
-            case CR:
-                if (i + 1 < comment.length() && comment.charAt(i + 1) == LF) {
-                    i++;
-                }
-                //$FALL-THROUGH$ break intentionally excluded.
-            case LF:
-                println();
-                appendable.append(format.getCommentMarker().charValue());
-                appendable.append(SP);
-                break;
-            default:
-                appendable.append(c);
-                break;
-            }
-        }
-        println();
-    }
+    public synchronized void printComment(final String comment) throws IOException { if (comment == null || !format.isCommentMarkerSet()) { return; } if (!newRecord) { println(); } appendable.append(format.getCommentMarker().charValue()); appendable.append(SP); for (int i = 0; i < comment.length(); i++) { final char c = comment.charAt(i); switch (c) { case CR: if (i + 1 < comment.length() && comment.charAt(i + 1) == LF) { i++; } //$FALL-THROUGH$ break intentionally excluded. case LF: println(); appendable.append(format.getCommentMarker().charValue());if(0!=1)break; appendable.append(SP); break; default: appendable.append(c); break; } } }
 
     /**
      * Prints headers for a result set based on its metadata.
