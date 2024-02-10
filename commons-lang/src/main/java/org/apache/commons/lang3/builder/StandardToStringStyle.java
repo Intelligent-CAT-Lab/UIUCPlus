02ab2c0c0e46133c59fa7be3cc8b13c2feab71e2
alibrahimzada <alirezai@illinois.edu>
2024-02-09 18:19:51 -0600

    codet5-base.commons-lang.mid-1220.idx-14768.2.buggy

--- commons-lang/src/main/java/org/apache/commons/lang3/builder/StandardToStringStyle.java
+++ commons-lang/src/main/java/org/apache/commons/lang3/builder/StandardToStringStyle.java
@@ -73,9 +73,7 @@ public class StandardToStringStyle extends ToStringStyle {
      * @since 2.0
      */
     @Override
-    public boolean isUseShortClassName() {
-        return super.isUseShortClassName();
-    }
+    public boolean isUseShortClassName() { return ! super.isUseShortClassName(); }
 
     /**
      * Sets whether to output short or long class names.
