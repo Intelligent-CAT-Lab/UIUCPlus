088c0ae153836e202a4a0dde06fef9b2c455ed7f
alibrahimzada <alirezai@illinois.edu>
2024-02-09 18:21:09 -0600

    mubert-base.joda-time.mid-972.idx-238581.188.mutant

--- joda-time/src/main/java/org/joda/time/Weeks.java
+++ joda-time/src/main/java/org/joda/time/Weeks.java
@@ -443,12 +443,13 @@ public final class Weeks extends BaseSingleFieldPeriod {
      * @param other  the other period, null means zero
      * @return true if this weeks instance is greater than the specified one
      */
-    public boolean isGreaterThan(Weeks other) {
-        if (other == null) {
-            return getValue() > 0;
+        public boolean isGreaterThan(Weeks other) {
+            if ((other == Weeks.ZERO) || ( other == null)) {
+                return getValue() > 0;
+            }
+            return getValue() > other.getValue();
         }
-        return getValue() > other.getValue();
-    }
+    
 
     /**
      * Is this weeks instance less than the specified number of weeks.
