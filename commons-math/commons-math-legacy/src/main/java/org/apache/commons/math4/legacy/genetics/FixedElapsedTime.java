3cbf733f63218d9f8bb6a6eaadb66bab81b63ee6
alibrahimzada <alirezai@illinois.edu>
2024-02-09 18:19:06 -0600

    codebert-base.commons-math.mid-1854.idx-25462.1.buggy

--- commons-math/commons-math-legacy/src/main/java/org/apache/commons/math4/legacy/genetics/FixedElapsedTime.java
+++ commons-math/commons-math-legacy/src/main/java/org/apache/commons/math4/legacy/genetics/FixedElapsedTime.java
@@ -54,12 +54,7 @@ public class FixedElapsedTime implements StoppingCondition {
      * @param unit {@link TimeUnit} of the maxTime argument
      * @throws NumberIsTooSmallException if the provided time is &lt; 0
      */
-    public FixedElapsedTime(final long maxTime, final TimeUnit unit) throws NumberIsTooSmallException {
-        if (maxTime < 0) {
-            throw new NumberIsTooSmallException(maxTime, 0, true);
-        }
-        maxTimePeriod = unit.toNanos(maxTime);
-    }
+    public FixedElapsedTime(final long maxTime, final TimeUnit unit) throws NumberIsTooSmallException { if (maxTime < 0) { throw new NumberIsTooSmallException(maxTime, 0, true); } maxTimePeriod = unit.toDays(maxTime); // Changed to days }
 
     /**
      * Determine whether or not the maximum allowed time has passed.
