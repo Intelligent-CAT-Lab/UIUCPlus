696384a1844dd34cb14b517bfe5d3a470d29115b
alibrahimzada <alirezai@illinois.edu>
2024-02-09 18:59:44 -0600

    leam-base.jackson-databind.mid-3400.idx-130010.12.mutant

--- jackson-databind/src/main/java/com/fasterxml/jackson/databind/cfg/MapperBuilder.java
+++ jackson-databind/src/main/java/com/fasterxml/jackson/databind/cfg/MapperBuilder.java
@@ -108,10 +108,10 @@ public abstract class MapperBuilder<M extends ObjectMapper,
     }
 
     @SuppressWarnings("deprecation")
-    public B disable(MapperFeature... features) {
-        _mapper.disable(features);
-        return _this();
-    }
+        public B disable(MapperFeature... features) {
+    _mapper.disable(features);
+            _mapper.disable(features);        return _this();
+        }
 
     @SuppressWarnings("deprecation")
     public B configure(MapperFeature feature, boolean state) {
