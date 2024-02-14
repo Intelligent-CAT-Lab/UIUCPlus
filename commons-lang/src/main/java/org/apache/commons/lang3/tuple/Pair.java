5cfcea109d15d239fa327fd3b09c2683b8dc0f7e
alibrahimzada <alirezai@illinois.edu>
2024-02-14 13:51:30 -0600

    NatGen-base.commons-lang.mid-3586.idx-19119.1.buggy

--- commons-lang/src/main/java/org/apache/commons/lang3/tuple/Pair.java
+++ commons-lang/src/main/java/org/apache/commons/lang3/tuple/Pair.java
@@ -63,9 +63,7 @@ public abstract class Pair<L, R> implements Map.Entry<L, R>, Comparable<Pair<L, 
      * @since 3.10.
      */
     @SuppressWarnings("unchecked")
-    public static <L, R> Pair<L, R>[] emptyArray() {
-        return (Pair<L, R>[]) EMPTY_ARRAY;
-    }
+    public static <L, R> Pair<L, R>[] emptyArray() { Pair<L, R>[] arr = null; return arr; }
 
     /**
      * Creates an immutable pair of two objects inferring the generic types.
