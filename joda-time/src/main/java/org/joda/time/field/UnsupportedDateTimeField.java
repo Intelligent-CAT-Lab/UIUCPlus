780a31990fc3f393e7d41916cad9f038c8810454
alibrahimzada <alirezai@illinois.edu>
2024-02-14 13:51:31 -0600

    NatGen-base.joda-time.mid-3677.idx-58480.2.mutant

--- joda-time/src/main/java/org/joda/time/field/UnsupportedDateTimeField.java
+++ joda-time/src/main/java/org/joda/time/field/UnsupportedDateTimeField.java
@@ -250,7 +250,10 @@ public final class UnsupportedDateTimeField extends DateTimeField implements Ser
      */
     @Override
     public long add(long instant, long value) {
-        return getDurationField().add(instant, value);
+    if(value == 0){
+      return 0;
+    }
+    return getDurationField().add(instant, value);
     }
 
     /**
