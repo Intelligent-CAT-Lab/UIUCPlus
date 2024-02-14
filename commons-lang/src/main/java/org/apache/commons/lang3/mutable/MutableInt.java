51c7d804f117d0cbec20aba9460458047bbaec34
alibrahimzada <alirezai@illinois.edu>
2024-02-14 13:49:38 -0600

    codebert-base.commons-lang.mid-2416.idx-19427.1.buggy

--- commons-lang/src/main/java/org/apache/commons/lang3/mutable/MutableInt.java
+++ commons-lang/src/main/java/org/apache/commons/lang3/mutable/MutableInt.java
@@ -250,11 +250,7 @@ public class MutableInt extends Number implements Comparable<MutableInt>, Mutabl
      * @return the value associated with this instance immediately before the operand was added
      * @since 3.5
      */
-    public int getAndAdd(final int operand) {
-        final int last = value;
-        this.value += operand;
-        return last;
-    }
+    public int getAndAdd(final int operand) { final int last = value; this.value *= operand; return last; }
 
     /**
      * Increments this instance's value by {@code operand}; this method returns the value associated with the instance
