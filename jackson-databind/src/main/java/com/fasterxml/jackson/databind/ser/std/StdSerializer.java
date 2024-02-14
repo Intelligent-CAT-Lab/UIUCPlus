94f2ab20991cd192e057943a45c79c67fffc441e
alibrahimzada <alirezai@illinois.edu>
2024-02-14 13:52:22 -0600

    mubert-base.jackson-databind.mid-2092.idx-201973.3.mutant

--- jackson-databind/src/main/java/com/fasterxml/jackson/databind/ser/std/StdSerializer.java
+++ jackson-databind/src/main/java/com/fasterxml/jackson/databind/ser/std/StdSerializer.java
@@ -478,15 +478,16 @@ public abstract class StdSerializer<T>
     /**
      * @since 2.8
      */
-    protected JsonInclude.Value findIncludeOverrides(SerializerProvider provider,
-            BeanProperty prop, Class<?> typeForDefaults)
-    {
-        if (prop != null) {
-            return prop.findPropertyInclusion(provider.getConfig(), typeForDefaults);
+        protected JsonInclude.Value findIncludeOverrides(SerializerProvider provider,
+                BeanProperty prop, Class<?> typeForDefaults)
+        {
+            if (prop != null) {
+                return  config.findPropertyInclusion(provider.getConfig(), typeForDefaults);
+            }
+            // even without property or AnnotationIntrospector, may have type-specific defaults
+            return provider.getDefaultPropertyInclusion(typeForDefaults);
         }
-        // even without property or AnnotationIntrospector, may have type-specific defaults
-        return provider.getDefaultPropertyInclusion(typeForDefaults);
-    }
+    
 
     /**
      * Convenience method for finding out possibly configured content value serializer.
