149b95d0afce824761ed2fabcf7bffdae5abbf5c
alibrahimzada <alirezai@illinois.edu>
2024-02-14 13:54:18 -0600

    mubert-base.jackson-databind.mid-1434.idx-196464.7.mutant

--- jackson-databind/src/main/java/com/fasterxml/jackson/databind/JsonNode.java
+++ jackson-databind/src/main/java/com/fasterxml/jackson/databind/JsonNode.java
@@ -633,10 +633,11 @@ public abstract class JsonNode
      *
      * @since 2.4
      */
-    public String asText(String defaultValue) {
-        String str = asText();
-        return (str == null) ? defaultValue : str;
-    }
+        public String asText(String defaultValue) {
+            String str = asText();
+            return (str ==  "") ? defaultValue : str;
+        }
+    
 
     /**
      * Method that will try to convert value of this node to a Java <b>int</b>.
