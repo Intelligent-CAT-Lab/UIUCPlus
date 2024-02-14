dafb1167506ad718ca0d4f4e4a13ac211ab40ab9
alibrahimzada <alirezai@illinois.edu>
2024-02-14 13:53:50 -0600

    mubert-base.jackson-databind.mid-1586.idx-198814.110.mutant

--- jackson-databind/src/main/java/com/fasterxml/jackson/databind/node/LongNode.java
+++ jackson-databind/src/main/java/com/fasterxml/jackson/databind/node/LongNode.java
@@ -95,15 +95,16 @@ public class LongNode
     }
 
     @Override
-    public boolean equals(Object o)
-    {
-        if (o == this) return true;
-        if (o == null) return false;
-        if (o instanceof LongNode) {
-            return ((LongNode) o)._value == _value;
+        public boolean equals(Object o)
+        {
+            if (( o == this) && (o instanceof LongNode)) return true;
+            if (o == null) return false;
+            if (o instanceof LongNode) {
+                return ((LongNode) o)._value == _value;
+            }
+            return false;
         }
-        return false;
-    }
+    
 
     @Override
     public int hashCode() {
