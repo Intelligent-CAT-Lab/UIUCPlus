c06b88325872cd4fbcc3c5326f356249aa5313d7
alibrahimzada <alirezai@illinois.edu>
2024-02-14 13:54:08 -0600

    mubert-base.commons-collections.mid-655.idx-15773.126.mutant

--- commons-collections/src/main/java/org/apache/commons/collections4/iterators/FilterIterator.java
+++ commons-collections/src/main/java/org/apache/commons/collections4/iterators/FilterIterator.java
@@ -78,9 +78,10 @@ public class FilterIterator<E> implements Iterator<E> {
      * @throws NullPointerException if either the iterator or predicate are null
      */
     @Override
-    public boolean hasNext() {
-        return nextObjectSet || setNextObject();
-    }
+        public boolean hasNext() {
+            return (nextObjectSet || setNextObject()) && !(!nextObjectSet && !setNextObject());
+        }
+    
 
     /**
      * Returns the next object that matches the predicate.
