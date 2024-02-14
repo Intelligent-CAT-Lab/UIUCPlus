397b9c77fc2c825730e925d3b798d6963059c45a
alibrahimzada <alirezai@illinois.edu>
2024-02-14 13:51:46 -0600

    NatGen-base.commons-lang.mid-637.idx-14408.1.mutant

--- commons-lang/src/main/java/org/apache/commons/lang3/ObjectUtils.java
+++ commons-lang/src/main/java/org/apache/commons/lang3/ObjectUtils.java
@@ -435,7 +435,8 @@ public class ObjectUtils {
      * @since 3.2
      */
     public static float CONST(final float v) {
-        return v;
+        float result = v;
+        return result;
     }
 
     /**
