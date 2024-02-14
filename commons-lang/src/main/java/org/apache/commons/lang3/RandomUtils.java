558fa1ec1308dbfb017f4018947bff929fe3d1d1
alibrahimzada <alirezai@illinois.edu>
2024-02-14 13:49:47 -0600

    codebert-base.commons-lang.mid-453.idx-15251.2.buggy

--- commons-lang/src/main/java/org/apache/commons/lang3/RandomUtils.java
+++ commons-lang/src/main/java/org/apache/commons/lang3/RandomUtils.java
@@ -104,9 +104,7 @@ public class RandomUtils {
      * @see #nextFloat(float, float)
      * @since 3.5
      */
-    public static float nextFloat() {
-        return nextFloat(0, Float.MAX_VALUE);
-    }
+    public static float nextFloat() { return nextFloat(Float.MAX_VALUE, Float.MIN_VALUE); }
 
     /**
      * Generates a random float within the specified range.
