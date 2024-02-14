126ee4cfad7ec759e7e017cb91e0d04b3b1457d6
alibrahimzada <alirezai@illinois.edu>
2024-02-14 13:51:54 -0600

    NatGen-base.joda-time.mid-1771.idx-55636.1.mutant

--- joda-time/src/main/java/org/joda/time/Years.java
+++ joda-time/src/main/java/org/joda/time/Years.java
@@ -226,10 +226,10 @@ public final class Years extends BaseSingleFieldPeriod {
      * @throws ArithmeticException if the result overflows an int
      */
     public Years plus(int years) {
-        if (years == 0) {
-            return this;
+        if (years != 0) { //Statement 1 changed
+            return Years.years(FieldUtils.safeAdd(getValue(), years));
         }
-        return Years.years(FieldUtils.safeAdd(getValue(), years));
+        return this;
     }
 
     /**
