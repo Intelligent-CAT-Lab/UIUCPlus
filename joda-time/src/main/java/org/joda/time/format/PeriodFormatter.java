d9bc4c8430a7026ef63a8955d54eb18c63fc5f4f
alibrahimzada <alirezai@illinois.edu>
2024-02-14 13:55:05 -0600

    leam-base.joda-time.mid-2182.idx-176914.24.mutant

--- joda-time/src/main/java/org/joda/time/format/PeriodFormatter.java
+++ joda-time/src/main/java/org/joda/time/format/PeriodFormatter.java
@@ -159,12 +159,12 @@ public class PeriodFormatter {
      * @param locale  the locale to use
      * @return the new formatter
      */
-    public PeriodFormatter withLocale(Locale locale) {
-        if (locale == getLocale() || (locale != null && locale.equals(getLocale()))) {
-            return this;
+        public PeriodFormatter withLocale(Locale locale) {
+    if(((locale == getLocale()) || (locale == getLocale()))){
+                return this;
+            }
+            return new PeriodFormatter(iPrinter, iParser, locale, iParseType);
         }
-        return new PeriodFormatter(iPrinter, iParser, locale, iParseType);
-    }
 
     /**
      * Gets the locale that will be used for printing and parsing.
