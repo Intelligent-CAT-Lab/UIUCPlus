9a7926927d0bfc71a6279d34f9df9160cafea6d1
alibrahimzada <alirezai@illinois.edu>
2024-02-14 13:50:34 -0600

    codet5-base.jackson-databind.mid-3974.idx-38195.1.mutant

--- jackson-databind/src/main/java/com/fasterxml/jackson/databind/introspect/AnnotatedMethod.java
+++ jackson-databind/src/main/java/com/fasterxml/jackson/databind/introspect/AnnotatedMethod.java
@@ -131,8 +131,8 @@ public final class AnnotatedMethod
     @Override
     public Class<?> getRawParameterType(int index)
     {
-        Class<?>[] types = getRawParameterTypes();
-        return (index >= types.length) ? null : types[index];
+    Class<?>[] types = getRawParameterTypes();
+    return (index > types.length) ? null : types[index]; // Change ">=" to ">"
     }
 
     @Override
