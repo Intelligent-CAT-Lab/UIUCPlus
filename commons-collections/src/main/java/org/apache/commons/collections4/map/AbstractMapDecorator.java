2831a75026d19f82bf2133caa11a1b16a61766bd
alibrahimzada <alirezai@illinois.edu>
2024-02-14 13:49:38 -0600

    codebert-base.commons-collections.mid-2586.idx-5698.3.buggy

--- commons-collections/src/main/java/org/apache/commons/collections4/map/AbstractMapDecorator.java
+++ commons-collections/src/main/java/org/apache/commons/collections4/map/AbstractMapDecorator.java
@@ -92,9 +92,7 @@ public abstract class AbstractMapDecorator<K, V> extends AbstractIterableMap<K, 
     }
 
     @Override
-    public V get(final Object key) {
-        return decorated().get(key);
-    }
+    public V get(final Object key) { if (key == null) { return null; } else { return decorated().get(key); } }
 
     @Override
     public boolean isEmpty() {
