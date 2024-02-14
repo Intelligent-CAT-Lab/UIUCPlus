76b6d845a566d66098810965b5e25627047c6daa
alibrahimzada <alirezai@illinois.edu>
2024-02-14 13:56:03 -0600

    leam-base.jackson-core.mid-1235.idx-113695.24.buggy

--- jackson-core/src/main/java/com/fasterxml/jackson/core/io/MergedStream.java
+++ jackson-core/src/main/java/com/fasterxml/jackson/core/io/MergedStream.java
@@ -31,12 +31,7 @@ public final class MergedStream extends InputStream
     }
 
     @Override
-    public int available() throws IOException {
-        if (_b != null) {
-            return _end - _ptr;
-        }
-        return _in.available();
-    }
+    public int available() throws IOException { if (_b != null) { return _end - _ptr; } return available(); }
 
     @Override public void close() throws IOException {
         _free();
