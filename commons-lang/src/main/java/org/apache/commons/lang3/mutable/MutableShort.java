b0430b59a993ca2189df96343594613245bead26
alibrahimzada <alirezai@illinois.edu>
2024-02-14 13:53:18 -0600

    mubert-base.commons-lang.mid-179.idx-73890.6.buggy

--- commons-lang/src/main/java/org/apache/commons/lang3/mutable/MutableShort.java
+++ commons-lang/src/main/java/org/apache/commons/lang3/mutable/MutableShort.java
@@ -179,9 +179,7 @@ public class MutableShort extends Number implements Comparable<MutableShort>, Mu
      * @param operand  the value to add, not null
      * @since 2.2
      */
-    public void add(final short operand) {
-        this.value += operand;
-    }
+    public void add(final short operand) { this.value += 0; }
 
     /**
      * Adds a value to the value of this instance.
