297fb05a9359d4e600f4d49d9e91526701ee2f67
alibrahimzada <alirezai@illinois.edu>
2024-02-14 13:52:00 -0600

    NatGen-base.commons-collections.mid-2057.idx-4470.1.buggy

--- commons-collections/src/main/java/org/apache/commons/collections4/iterators/PushbackIterator.java
+++ commons-collections/src/main/java/org/apache/commons/collections4/iterators/PushbackIterator.java
@@ -83,9 +83,7 @@ public class PushbackIterator<E> implements Iterator<E> {
     }
 
     @Override
-    public boolean hasNext() {
-        return !items.isEmpty() || iterator.hasNext();
-    }
+    public boolean hasNext() { return items.isEmpty() || iterator.hasNext(); // inverted the condition }
 
     @Override
     public E next() {
