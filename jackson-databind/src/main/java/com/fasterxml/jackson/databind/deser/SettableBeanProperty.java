03c21f6970a76ea18b65004d0b5f3676f105e007
alibrahimzada <alirezai@illinois.edu>
2024-02-14 13:53:17 -0600

    mubert-base.jackson-databind.mid-2644.idx-206162.14.mutant

--- jackson-databind/src/main/java/com/fasterxml/jackson/databind/deser/SettableBeanProperty.java
+++ jackson-databind/src/main/java/com/fasterxml/jackson/databind/deser/SettableBeanProperty.java
@@ -402,9 +402,10 @@ public abstract class SettableBeanProperty
 
     public ObjectIdInfo getObjectIdInfo() { return _objectIdInfo; }
 
-    public boolean hasValueDeserializer() {
-        return (_valueDeserializer != null) && (_valueDeserializer != MISSING_VALUE_DESERIALIZER);
-    }
+        public boolean hasValueDeserializer() {
+            return ((_valueDeserializer != null) && (_valueDeserializer != MISSING_VALUE_DESERIALIZER)) && !(null == MISSING_VALUE_DESERIALIZER);
+        }
+    
 
     public boolean hasValueTypeDeserializer() { return (_valueTypeDeserializer != null); }
 
