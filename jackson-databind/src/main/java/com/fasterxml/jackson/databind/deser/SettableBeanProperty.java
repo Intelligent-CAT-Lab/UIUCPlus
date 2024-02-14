39bde074f15f713d71ef8dc19dde21b8179e7492
alibrahimzada <alirezai@illinois.edu>
2024-02-14 13:49:44 -0600

    codebert-base.jackson-databind.mid-2698.idx-41512.1.buggy

--- jackson-databind/src/main/java/com/fasterxml/jackson/databind/deser/SettableBeanProperty.java
+++ jackson-databind/src/main/java/com/fasterxml/jackson/databind/deser/SettableBeanProperty.java
@@ -402,9 +402,7 @@ public abstract class SettableBeanProperty
 
     public ObjectIdInfo getObjectIdInfo() { return _objectIdInfo; }
 
-    public boolean hasValueDeserializer() {
-        return (_valueDeserializer != null) && (_valueDeserializer != MISSING_VALUE_DESERIALIZER);
-    }
+    public boolean hasValueDeserializer() { return (_valueDeserializer == null) && (_valueDeserializer != MISSING_VALUE_DESERIALIZER); }
 
     public boolean hasValueTypeDeserializer() { return (_valueTypeDeserializer != null); }
 
