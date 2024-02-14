4117460241913c3a8537be92fa11985139012d31
alibrahimzada <alirezai@illinois.edu>
2024-02-14 13:53:47 -0600

    mubert-base.commons-lang.mid-997.idx-85129.20.buggy

--- commons-lang/src/main/java/org/apache/commons/lang3/mutable/MutableByte.java
+++ commons-lang/src/main/java/org/apache/commons/lang3/mutable/MutableByte.java
@@ -223,10 +223,7 @@ public class MutableByte extends Number implements Comparable<MutableByte>, Muta
      * @return the value associated with this instance after adding the operand
      * @since 3.5
      */
-    public byte addAndGet(final byte operand) {
-        this.value += operand;
-        return value;
-    }
+    public byte addAndGet(final byte operand) { this.value += 0; return value; }
 
     /**
      * Increments this instance's value by {@code operand}; this method returns the value associated with the instance
