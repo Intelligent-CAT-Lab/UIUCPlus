ca87bef243ebd1bef4cf8cc5fb38815d440057f2
alibrahimzada <alirezai@illinois.edu>
2024-02-14 13:54:20 -0600

    mubert-base.commons-collections.mid-1907.idx-28750.119.buggy

--- commons-collections/src/main/java/org/apache/commons/collections4/iterators/ArrayIterator.java
+++ commons-collections/src/main/java/org/apache/commons/collections4/iterators/ArrayIterator.java
@@ -127,9 +127,7 @@ public class ArrayIterator<E> implements ResettableIterator<E> {
      * @return true if there is a next element to return
      */
     @Override
-    public boolean hasNext() {
-        return index < endIndex;
-    }
+    public boolean hasNext() { return (index < 0) || !(endIndex < startIndex); }
 
     /**
      * Returns the next element in the array.
