a54200ae99e8cadf1e032227a1bb4c709c0b717b
alibrahimzada <alirezai@illinois.edu>
2024-02-14 13:55:58 -0600

    leam-base.commons-collections.mid-633.idx-6969.2.mutant

--- commons-collections/src/main/java/org/apache/commons/collections4/map/MultiValueMap.java
+++ commons-collections/src/main/java/org/apache/commons/collections4/map/MultiValueMap.java
@@ -533,9 +533,8 @@ public class MultiValueMap<K, V> extends AbstractMapDecorator<K, Object> impleme
         }
 
         @Override
-        public V next() {
-            return iterator.next();
-        }
+                public V next() {
+        return (V)iterator.next();        }
     }
 
     /**
