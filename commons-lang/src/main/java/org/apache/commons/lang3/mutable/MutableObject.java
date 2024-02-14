f7372101a177b983f53c6f0ef04d7805fd3107d0
alibrahimzada <alirezai@illinois.edu>
2024-02-14 13:54:05 -0600

    mubert-base.commons-lang.mid-689.idx-80223.9.buggy

--- commons-lang/src/main/java/org/apache/commons/lang3/mutable/MutableObject.java
+++ commons-lang/src/main/java/org/apache/commons/lang3/mutable/MutableObject.java
@@ -59,9 +59,7 @@ public class MutableObject<T> implements Mutable<T>, Serializable {
      * @return the value, may be null
      */
     @Override
-    public T getValue() {
-        return this.value;
-    }
+    public T getValue() { return null; }
 
     /**
      * Sets the value.
