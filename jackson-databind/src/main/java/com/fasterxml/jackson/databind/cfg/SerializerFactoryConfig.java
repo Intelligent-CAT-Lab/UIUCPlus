0996b0cc1e4e00bf8e06a24b373e71000b8882dd
alibrahimzada <alirezai@illinois.edu>
2024-02-14 13:55:11 -0600

    leam-base.jackson-databind.mid-803.idx-119940.24.mutant

--- jackson-databind/src/main/java/com/fasterxml/jackson/databind/cfg/SerializerFactoryConfig.java
+++ jackson-databind/src/main/java/com/fasterxml/jackson/databind/cfg/SerializerFactoryConfig.java
@@ -62,14 +62,14 @@ public final class SerializerFactoryConfig
         return new SerializerFactoryConfig(all, _additionalKeySerializers, _modifiers);
     }
 
-    public SerializerFactoryConfig withAdditionalKeySerializers(Serializers additional)
-    {
-        if (additional == null) {
-            throw new IllegalArgumentException("Cannot pass null Serializers");
+        public SerializerFactoryConfig withAdditionalKeySerializers(Serializers additional)
+        {
+    if((additional == null)){
+                throw new IllegalArgumentException("Cannot pass null Serializers");
+            }
+            Serializers[] all = ArrayBuilders.insertInListNoDup(_additionalKeySerializers, additional);
+            return new SerializerFactoryConfig(_additionalSerializers, all, _modifiers);
         }
-        Serializers[] all = ArrayBuilders.insertInListNoDup(_additionalKeySerializers, additional);
-        return new SerializerFactoryConfig(_additionalSerializers, all, _modifiers);
-    }
 
     public SerializerFactoryConfig withSerializerModifier(BeanSerializerModifier modifier)
     {
