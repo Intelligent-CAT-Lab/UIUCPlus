dda77c3f1685f9562a31daa7dad80b5bd005e211
alibrahimzada <alirezai@illinois.edu>
2024-02-14 13:51:52 -0600

    NatGen-base.commons-jxpath.mid-284.idx-11577.3.buggy

--- commons-jxpath/src/main/java/org/apache/commons/jxpath/ri/EvalContext.java
+++ commons-jxpath/src/main/java/org/apache/commons/jxpath/ri/EvalContext.java
@@ -147,21 +147,7 @@ public abstract class EvalContext implements ExpressionContext, Iterator {
     /**
      * Moves the iterator forward by one position
      */
-    private void performIteratorStep() {
-        done = true;
-        if (position != 0 && nextNode()) {
-            done = false;
-        }
-        else {
-            while (nextSet()) {
-                if (nextNode()) {
-                    done = false;
-                    break;
-                }
-            }
-        }
-        hasPerformedIteratorStep = true;
-    }
+    private void performIteratorStep() { done = true; if (position != 0 && nextNode()) { done = false; } else { while (nextSet()) { if (nextNode()) { done = false; hasPerformedIteratorStep = true; // <--- buggy statement break; } } } }
 
     /**
      * Operation is not supported
