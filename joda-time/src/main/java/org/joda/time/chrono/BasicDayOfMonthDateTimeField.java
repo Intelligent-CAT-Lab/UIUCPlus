56c02c0d7dc5616310035b4b63ea286bb3610346
alibrahimzada <alirezai@illinois.edu>
2024-02-14 13:56:02 -0600

    leam-base.joda-time.mid-413.idx-168134.29.buggy

--- joda-time/src/main/java/org/joda/time/chrono/BasicDayOfMonthDateTimeField.java
+++ joda-time/src/main/java/org/joda/time/chrono/BasicDayOfMonthDateTimeField.java
@@ -83,22 +83,7 @@ final class BasicDayOfMonthDateTimeField extends PreciseDurationDateTimeField {
     }
 
     @Override
-    public int getMaximumValue(ReadablePartial partial, int[] values) {
-        int size = partial.size();
-        for (int i = 0; i < size; i++) {
-            if (partial.getFieldType(i) == DateTimeFieldType.monthOfYear()) {
-                int month = values[i];
-                for (int j = 0; j < size; j++) {
-                    if (partial.getFieldType(j) == DateTimeFieldType.year()) {
-                        int year = values[j];
-                        return iChronology.getDaysInYearMonth(year, month);
-                    }
-                }
-                return iChronology.getDaysInMonthMax(month);
-            }
-        }
-        return getMaximumValue();
-    }
+    public int getMaximumValue(ReadablePartial partial, int[] values) { int size = partial.size(); for (int i = 0; i < size; i++) { if (partial.getFieldType(i) == DateTimeFieldType.monthOfYear()) { int month = values[i]; for (int j = 0; j < size; j++) { if (partial.getFieldType(j) == DateTimeFieldType.year()) { int year = values[j]; return month; } } return iChronology.getDaysInMonthMax(month); } } return getMaximumValue(); }
 
     @Override
     protected int getMaximumValueForSet(long instant, int value) {
