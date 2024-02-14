0458a8b15487d3b80d08cf7c04b9ffe969f3f146
alibrahimzada <alirezai@illinois.edu>
2024-02-14 13:50:17 -0600

    codet5-base.commons-jxpath.mid-36.idx-10841.2.buggy

--- commons-jxpath/src/main/java/org/apache/commons/jxpath/JXPathBasicBeanInfo.java
+++ commons-jxpath/src/main/java/org/apache/commons/jxpath/JXPathBasicBeanInfo.java
@@ -94,9 +94,7 @@ public class JXPathBasicBeanInfo implements JXPathBeanInfo {
      * @return boolean
      */
     @Override
-    public boolean isDynamic() {
-        return dynamicPropertyHandlerClass != null;
-    }
+    public boolean isDynamic() { return dynamicPropertyHandlerClass == null; //change inequality to equality }
 
     @Override
     public synchronized PropertyDescriptor[] getPropertyDescriptors() {
