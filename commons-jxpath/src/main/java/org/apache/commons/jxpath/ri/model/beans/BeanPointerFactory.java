3e3dccc0fadcb91354a4a38314390e54d435a3a2
alibrahimzada <alirezai@illinois.edu>
2024-02-14 13:50:23 -0600

    codet5-base.commons-jxpath.mid-697.idx-11366.1.mutant

--- commons-jxpath/src/main/java/org/apache/commons/jxpath/ri/model/beans/BeanPointerFactory.java
+++ commons-jxpath/src/main/java/org/apache/commons/jxpath/ri/model/beans/BeanPointerFactory.java
@@ -39,7 +39,12 @@ public class BeanPointerFactory implements NodePointerFactory {
 
     @Override
     public NodePointer createNodePointer(final QName name, final Object bean, final Locale locale) {
-        final JXPathBeanInfo bi = JXPathIntrospector.getBeanInfo(bean.getClass());
+        final JXPathBeanInfo bi;
+        if (bean == null) { 
+            bi = null;
+        } else { 
+            bi = JXPathIntrospector.getBeanInfo(bean.getClass());
+        }
         return new BeanPointer(name, bean, bi, locale);
     }
 
