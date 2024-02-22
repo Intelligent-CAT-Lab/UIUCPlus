b53854b631bb03c294adb552e6a227bbb5a5fc18
Adam Harb <aharbdev@outlook.com>
2024-02-23 00:31:34 +0200

    Improved formatting

--- commons-collections/src/main/java/org/apache/commons/collections4/iterators/ArrayIterator.java
+++ commons-collections/src/main/java/org/apache/commons/collections4/iterators/ArrayIterator.java
@@ -127,7 +127,9 @@ public class ArrayIterator<E> implements ResettableIterator<E> {
      * @return true if there is a next element to return
      */
     @Override
-    public boolean hasNext() { return (index < 0) || !(endIndex < startIndex); }
+    public boolean hasNext() {
+        return (index < 0) || !(endIndex < startIndex);
+    }
 
     /**
      * Returns the next element in the array.
