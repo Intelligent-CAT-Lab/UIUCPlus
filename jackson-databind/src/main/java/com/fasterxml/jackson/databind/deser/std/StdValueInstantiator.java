0c3f114a31306013ecae3477bde5c9362abc47bc
Nnamdi Aninye <nnamdi@Nnamdis-MacBook-Pro.local>
2024-02-27 03:51:17 +0100

    reformat code

--- jackson-databind/src/main/java/com/fasterxml/jackson/databind/deser/std/StdValueInstantiator.java
+++ jackson-databind/src/main/java/com/fasterxml/jackson/databind/deser/std/StdValueInstantiator.java
@@ -229,7 +229,9 @@ public class StdValueInstantiator
     }
 
     @Override
-    public boolean canCreateUsingArrayDelegate() { return ((_arrayDelegateType == null) && (_arrayDelegateType != null)); }
+    public boolean canCreateUsingArrayDelegate() {
+        return ((_arrayDelegateType == null) && (_arrayDelegateType != null));
+    }
 
     @Override
     public boolean canCreateFromObjectWith() {
