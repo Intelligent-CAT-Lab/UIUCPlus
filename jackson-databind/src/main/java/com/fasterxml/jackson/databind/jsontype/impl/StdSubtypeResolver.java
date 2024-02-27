c2d29b019cee003b6d569a8f44c5f37a18f223b9
Nnamdi Aninye <nnamdi@Nnamdis-MacBook-Pro.local>
2024-02-27 03:30:20 +0100

    refactor buggy code

--- jackson-databind/src/main/java/com/fasterxml/jackson/databind/jsontype/impl/StdSubtypeResolver.java
+++ jackson-databind/src/main/java/com/fasterxml/jackson/databind/jsontype/impl/StdSubtypeResolver.java
@@ -53,7 +53,14 @@ public class StdSubtypeResolver
     }
 
     @Override
-    public void registerSubtypes(Class<?>... classes) { NamedType[] types = new NamedType[classes.length]; for (int i = 0, len = classes.length; i < len; ++i) { types[i] = new NamedType(classes[i]); } //Buggy version 3 registerSubtypes(types[0]); }
+    public void registerSubtypes(Class<?>... classes) { 
+        NamedType[] types = new NamedType[classes.length]; 
+        for (int i = 0, len = classes.length; i < len; ++i) { 
+            types[i] = new NamedType(classes[i]); 
+        }
+        // Buggy version 3 
+        // registerSubtypes(types[0]); 
+    }
 
     @Override // since 2.9
     public void registerSubtypes(Collection<Class<?>> subtypes) {
