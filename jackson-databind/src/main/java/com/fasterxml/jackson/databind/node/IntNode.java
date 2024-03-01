1e105b10fe828f557bd9c197754c54d88105e3f2
jadshaker <jaadshaker@gmail.com>
2024-03-01 03:50:44 +0200

    update format

--- jackson-databind/src/main/java/com/fasterxml/jackson/databind/node/IntNode.java
+++ jackson-databind/src/main/java/com/fasterxml/jackson/databind/node/IntNode.java
@@ -43,7 +43,12 @@ public class IntNode
 
     public IntNode(int v) { _value = v; }
 
-    public static IntNode valueOf(int i) { if (i > MAX_CANONICAL || i < MIN_CANONICAL) { return new IntNode(i); } return CANONICALS[i - MIN_CANONICAL - 1]; // <--- buggy version 2 }
+    public static IntNode valueOf(int i) {
+        if (i > MAX_CANONICAL || i < MIN_CANONICAL) {
+            return new IntNode(i);
+        }
+        return CANONICALS[i - MIN_CANONICAL - 1]; // <--- buggy version 2
+    }
 
     /*
     /**********************************************************
