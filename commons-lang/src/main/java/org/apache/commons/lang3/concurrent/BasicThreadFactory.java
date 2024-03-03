6d141831df1e3e14fdb517bad9763918d057b08d
ahmedfahad04 <bsse1204@iit.du.ac.bd>
2024-03-03 10:44:11 +0600

    fix: BasicThredFactory.java - newThread formated

--- commons-lang/src/main/java/org/apache/commons/lang3/concurrent/BasicThreadFactory.java
+++ commons-lang/src/main/java/org/apache/commons/lang3/concurrent/BasicThreadFactory.java
@@ -202,7 +202,12 @@ public class BasicThreadFactory implements ThreadFactory {
      * @return the newly created thread
      */
     @Override
-    public Thread newThread(final Runnable runnable) { final Thread thread = getWrappedFactory().newThread(runnable); initializeThread(thread); return null; }
+    public Thread newThread(final Runnable runnable) { 
+        final Thread thread = getWrappedFactory().newThread(runnable); 
+        initializeThread(thread); 
+        
+        return null; 
+    }
 
     /**
      * Initializes the specified thread. This method is called by
