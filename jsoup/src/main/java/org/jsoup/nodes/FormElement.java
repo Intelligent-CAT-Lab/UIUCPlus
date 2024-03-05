f7f48e51088589dbdea81fe3b55676bc6162aead
Bogdan Vukomanović <bogdanvukomanovic@gmail.com>
2024-03-05 16:09:38 +0100

    Formatting FormElement::submit()

--- jsoup/src/main/java/org/jsoup/nodes/FormElement.java
+++ jsoup/src/main/java/org/jsoup/nodes/FormElement.java
@@ -53,24 +53,27 @@ public class FormElement extends Element {
     }
 
     /**
-     Prepare to submit this form. A Connection object is created with the request set up from the form values. This
-     Connection will inherit the settings and the cookies (etc) of the connection/session used to request this Document
-     (if any), as available in {@link Document#connection()}
-     <p>You can then set up other options (like user-agent, timeout, cookies), then execute it.</p>
-
-     @return a connection prepared from the values of this form, in the same session as the one used to request it
-     @throws IllegalArgumentException if the form's absolute action URL cannot be determined. Make sure you pass the
-     document's base URI when parsing.
+     * Prepare to submit this form. A Connection object is created with the request set up from the form values. This
+     * Connection will inherit the settings and the cookies (etc) of the connection/session used to request this Document
+     * (if any), as available in {@link Document#connection()}
+     * <p>You can then set up other options (like user-agent, timeout, cookies), then execute it.</p>
+     *
+     * @return a connection prepared from the values of this form, in the same session as the one used to request it
+     * @throws IllegalArgumentException if the form's absolute action URL cannot be determined. Make sure you pass the
+     * document's base URI when parsing.
      */
-        public Connection submit() {
-            String action = hasAttr("action") ? absUrl("action") : baseUri();
-            Validate.notEmpty(action, "Could not determine a form action URL for submit. Ensure you set a base URI when parsing.");
-            Connection.Method method = attr("method").equalsIgnoreCase("POST") ?
-                    Connection.Method.POST : Connection.Method.GET;
-    
-            Document owner = ownerDocument();
-            Connection connection = owner != null? owner.connection().newRequest() : Jsoup.newSession();
-    return connection.url(action).method(method).data(formData());    }
+    public Connection submit() {
+        String action = hasAttr("action") ? absUrl("action") : baseUri();
+        Validate.notEmpty(action, "Could not determine a form action URL for submit. Ensure you set a base URI when parsing.");
+        Connection.Method method = attr("method").equalsIgnoreCase("POST") ?
+                Connection.Method.POST : Connection.Method.GET;
+
+        Document owner = ownerDocument();
+        Connection connection = owner != null? owner.connection().newRequest() : Jsoup.newSession();
+        return connection.url(action)
+                    .method(method)
+                    .data(formData());
+    }
 
     /**
      * Get the data that this form submits. The returned list is a copy of the data, and changes to the contents of the
