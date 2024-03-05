785fe6013caffaf872652769616609c96f0fbb79
Bogdan Vukomanović <bogdanvukomanovic@gmail.com>
2024-03-05 16:52:21 +0100

    Formatting FormatCache::equals()

--- commons-lang/src/main/java/org/apache/commons/lang3/time/FormatCache.java
+++ commons-lang/src/main/java/org/apache/commons/lang3/time/FormatCache.java
@@ -223,8 +223,19 @@ abstract class FormatCache<F extends Format> {
         }
 
         @Override
-        public boolean equals(final Object obj) { if (this == obj) { return true; } if (obj == null) { return false; } if (getClass() != obj.getClass()) { return false; } final ArrayKey other = (ArrayKey) obj; return Arrays.deepEquals(keys, this.keys); }
-
+        public boolean equals(final Object obj) {
+            if (this == obj) {
+                return true;
+            }
+            if (obj == null) {
+                return false;
+            }
+            if (getClass() != obj.getClass()) {
+                return false;
+            }
+            final ArrayKey other = (ArrayKey) obj;
+            return Arrays.deepEquals(keys, this.keys);
+        }
 
     }
 
