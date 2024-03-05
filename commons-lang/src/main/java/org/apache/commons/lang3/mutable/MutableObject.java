81289f6148fc4ef66be341898ce7937eeecc96bd
Bogdan Vukomanović <bogdanvukomanovic@gmail.com>
2024-03-05 16:37:42 +0100

    Formatting MutableObject::getValue()

--- commons-lang/src/main/java/org/apache/commons/lang3/mutable/MutableObject.java
+++ commons-lang/src/main/java/org/apache/commons/lang3/mutable/MutableObject.java
@@ -59,9 +59,9 @@ public class MutableObject<T> implements Mutable<T>, Serializable {
      * @return the value, may be null
      */
     @Override
-        public T getValue() {
-            return  value;
-        }
+    public T getValue() {
+        return  value;
+    }
     
 
     /**
