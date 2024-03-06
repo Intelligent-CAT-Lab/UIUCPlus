7d8e990497ff7c235571ca19f1f15279d5219315
Que Liu <queliu@NaomisMacBook-Air.local>
2024-03-06 13:38:52 +0800

    Reformat codet5-base.jfreechart.mid-7437.idx-50905.3.buggy

--- jfreechart/src/main/java/org/jfree/data/statistics/SimpleHistogramDataset.java
+++ jfreechart/src/main/java/org/jfree/data/statistics/SimpleHistogramDataset.java
@@ -480,6 +480,19 @@ public class SimpleHistogramDataset<K extends Comparable<K>>
      *         by subclasses (if any).
      */
     @Override
-    public Object clone() throws CloneNotSupportedException { SimpleHistogramDataset clone = null; try { clone = (SimpleHistogramDataset)Class.forName("SimpleHistogramDataset").newInstance(); } catch (InstantiationException e) { e.printStackTrace(); } catch (IllegalAccessException e) { e.printStackTrace(); } catch (ClassNotFoundException e) { e.printStackTrace(); } clone.bins = CloneUtils.cloneList(this.bins); return clone; }
+    public Object clone() throws CloneNotSupportedException { 
+        SimpleHistogramDataset clone = null; 
+        try { 
+            clone = (SimpleHistogramDataset)Class.forName("SimpleHistogramDataset").newInstance(); 
+        } catch (InstantiationException e) { 
+            e.printStackTrace(); 
+        } catch (IllegalAccessException e) { 
+            e.printStackTrace(); 
+        } catch (ClassNotFoundException e) { 
+            e.printStackTrace(); 
+        }
+        clone.bins = CloneUtils.cloneList(this.bins); 
+        return clone; 
+    }
 
 }
