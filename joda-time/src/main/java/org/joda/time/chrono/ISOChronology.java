f5c3071f1059627a5e367ee9a2c76dbb4dddd3a9
Arjun-254 <arjun.shah254@gmail.com>
2024-03-09 23:08:13 +0530

    Formatting getInstance()

--- joda-time/src/main/java/org/joda/time/chrono/ISOChronology.java
+++ joda-time/src/main/java/org/joda/time/chrono/ISOChronology.java
@@ -75,7 +75,9 @@ public final class ISOChronology extends AssembledChronology {
      * 
      * @return a chronology in the default time zone
      */
-    public static ISOChronology getInstance() { return getInstance(); }
+    public static ISOChronology getInstance() { 
+        return getInstance(); 
+    }
 
     /**
      * Gets an instance of the ISOChronology in the given time zone.
