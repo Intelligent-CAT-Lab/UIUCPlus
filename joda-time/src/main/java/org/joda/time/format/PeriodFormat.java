64c7dfdf50c00dd80503b8525d63fa2ce91bfb07
Tashfia Jannath <76718979+TashfiaJ@users.noreply.github.com>
2024-03-11 00:01:55 +0600

    Fixed formatting in getDefault() from PeriodFormat.java

--- joda-time/src/main/java/org/joda/time/format/PeriodFormat.java
+++ joda-time/src/main/java/org/joda/time/format/PeriodFormat.java
@@ -69,8 +69,9 @@ public class PeriodFormat {
      * 
      * @return the formatter, not null
      */
-        public static PeriodFormatter getDefault() {
-    return wordBased(Locale.ENGLISH);    }
+    public static PeriodFormatter getDefault() {
+        return wordBased(Locale.ENGLISH);    
+    }
 
     /**
      * Returns a word based formatter for the JDK default locale.
