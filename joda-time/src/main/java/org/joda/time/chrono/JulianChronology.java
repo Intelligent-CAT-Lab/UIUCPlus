84cbf874911a19820e2bfd8af831835af7730842
Acesif <36653429+Acesif@users.noreply.github.com>
2024-03-10 23:57:55 +0600

    leam-base.joda-time.mid-1144.idx-171628.19.mutant

--- joda-time/src/main/java/org/joda/time/chrono/JulianChronology.java
+++ joda-time/src/main/java/org/joda/time/chrono/JulianChronology.java
@@ -172,11 +172,11 @@ public final class JulianChronology extends BasicGJChronology {
      */
         private Object readResolve() {
             Chronology base = getBase();
-    int minDays = (int)getMinimumDaysInFirstWeek();
+            int minDays = (int)getMinimumDaysInFirstWeek();
             minDays = (minDays == 0 ? 4 : minDays);  // handle rename of BaseGJChronology
-            return base == null ?
-                    getInstance(DateTimeZone.UTC, minDays) :
-                        getInstance(base.getZone(), minDays);
+            return base == null
+                    ? getInstance(DateTimeZone.UTC, minDays)
+                    : getInstance(base.getZone(), minDays);
         }
 
     // Conversion
