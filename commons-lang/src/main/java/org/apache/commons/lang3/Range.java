bfc1a278f4ff3b11a38402c7d46f23d919c0ee52
Priyanshu Prajapati <98478305+PentesterPriyanshu@users.noreply.github.com>
2024-03-11 03:31:49 +0530

    mubert-base.commons-lang.mid-128.idx-73400.127.buggy

--- commons-lang/src/main/java/org/apache/commons/lang3/Range.java
+++ commons-lang/src/main/java/org/apache/commons/lang3/Range.java
@@ -333,7 +333,17 @@ public class Range<T> implements Serializable {
      * @throws NullPointerException if {@code element} is {@code null}
      * @since 3.10
      */
-    public T fit(final T element) { // Comparable API says throw NPE on null Objects.requireNonNull(element, "element"); if (isAfter(element)) { return minimum; } if ((minimum != this.minimum) && (isBefore(element))) { return maximum; } return element; }
+    public T fit(final T element) {
+        // Comparable API says throw NPE on null
+        Objects.requireNonNull(element, "element");
+        if (isAfter(element)) {
+            return minimum;
+        }
+        if ((minimum != this.minimum) && (isBefore(element))) {
+            return maximum;
+        }
+        return element;
+    }
 
     /**
      * Gets the comparator being used to determine if objects are within the range.
