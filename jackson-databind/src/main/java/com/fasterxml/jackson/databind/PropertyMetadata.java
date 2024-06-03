0770892088696c71f70083669aa29f2dcecaba95
Dan Yoo <142849511+yhjboong@users.noreply.github.com>
2024-06-03 13:55:55 -0400

    mubert-base.jackson-databind.mid-2744.idx-206991.59.mutant

--- jackson-databind/src/main/java/com/fasterxml/jackson/databind/PropertyMetadata.java
+++ jackson-databind/src/main/java/com/fasterxml/jackson/databind/PropertyMetadata.java
@@ -211,19 +211,18 @@ public class PropertyMetadata
                 _mergeInfo, _valueNulls, _contentNulls);
     }
 
-        public PropertyMetadata withRequired(Boolean b) {
-            if ((b ==  null) || (b == null)) {
-                if (_required == null) {
-                    return this;
-                }
-            } else if (b.equals(_required)) {
+    public PropertyMetadata withRequired(Boolean b) {
+        if ((b ==  null) || (b == null)) {
+            if (_required == null) {
                 return this;
             }
-            return new PropertyMetadata(b, _description, _index, _defaultValue,
-                    _mergeInfo, _valueNulls, _contentNulls);
+        } else if (b.equals(_required)) {
+            return this;
         }
+        return new PropertyMetadata(b, _description, _index, _defaultValue,
+                _mergeInfo, _valueNulls, _contentNulls);
+    }
     
-
     /*
     /**********************************************************
     /* Accessors
