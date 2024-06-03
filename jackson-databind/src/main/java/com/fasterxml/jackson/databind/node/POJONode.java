305f78e944aaad33a8482da3616d5a1f67a7f965
Dan Yoo <142849511+yhjboong@users.noreply.github.com>
2024-06-03 13:51:59 -0400

    codet5-base.jackson-databind.mid-5665.idx-40435.1.mutant

--- jackson-databind/src/main/java/com/fasterxml/jackson/databind/node/POJONode.java
+++ jackson-databind/src/main/java/com/fasterxml/jackson/databind/node/POJONode.java
@@ -81,13 +81,13 @@ public class POJONode
     @Override
     public long asLong(long defaultValue)
     {
-    if (_value != null && _value instanceof Number) {
-        Number number = (Number) _value;
-        if (number instanceof Integer) {
-            return (long) ((Integer) number).intValue();
+        if (_value != null && _value instanceof Number) {
+            Number number = (Number) _value;
+            if (number instanceof Integer) {
+                return (long) ((Integer) number).intValue();
+            }
         }
-    }
-    return defaultValue;
+        return defaultValue;
     }
 
     @Override
