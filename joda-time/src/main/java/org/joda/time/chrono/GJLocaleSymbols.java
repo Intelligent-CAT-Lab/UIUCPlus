ff28dc87dfa418e9e9e489779bc82d5b1f882561
Dan Yoo <142849511+yhjboong@users.noreply.github.com>
2024-06-20 19:03:40 -0400

    leam-base.joda-time.mid-1189.idx-172026.1.mutant

--- joda-time/src/main/java/org/joda/time/chrono/GJLocaleSymbols.java
+++ joda-time/src/main/java/org/joda/time/chrono/GJLocaleSymbols.java
@@ -165,8 +165,9 @@ class GJLocaleSymbols {
         iMaxHalfdayLength = maxLength(iHalfday);
     }
 
-        public String eraValueToText(int value) {
-    return (String)iEras[value];    }
+    public String eraValueToText(int value) {
+        return (String)iEras[value];
+    }
 
     public int eraTextToValue(String text) {
         Integer era = iParseEras.get(text);
