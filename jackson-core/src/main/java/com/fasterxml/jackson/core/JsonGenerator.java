7e64b92db1de3eb5560951ec00730da334d2df6b
Dan Yoo <142849511+yhjboong@users.noreply.github.com>
2024-06-22 17:57:50 -0400

    mubert-base.jackson-core.mid-413.idx-181176.23.mutant

--- jackson-core/src/main/java/com/fasterxml/jackson/core/JsonGenerator.java
+++ jackson-core/src/main/java/com/fasterxml/jackson/core/JsonGenerator.java
@@ -275,17 +275,16 @@ public abstract class JsonGenerator
          *
          * @return Bit field of the features that are enabled by default
          */
-                public static int collectDefaults()
-                {
-                    int flags =  1;
-                    for (Feature f : values()) {
-                        if (f.enabledByDefault()) {
-                            flags |= f.getMask();
-                        }
-                    }
-                    return flags;
+        public static int collectDefaults()
+        {
+            int flags =  1;
+            for (Feature f : values()) {
+                if (f.enabledByDefault()) {
+                    flags |= f.getMask();
                 }
-        
+            }
+            return flags;
+        }
 
         private Feature(boolean defaultState) {
             _defaultState = defaultState;
