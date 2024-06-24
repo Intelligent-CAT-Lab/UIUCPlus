29aa9552079a0f92d8155e2bb2ed240ffe7e8493
maryamabdou <maryamabdou2000@gmail.com>
2024-06-25 00:57:06 +0200

    added indentation and fixed the formatting

--- jackson-databind/src/main/java/com/fasterxml/jackson/databind/deser/std/UUIDDeserializer.java
+++ jackson-databind/src/main/java/com/fasterxml/jackson/databind/deser/std/UUIDDeserializer.java
@@ -10,21 +10,24 @@ import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 
-public class UUIDDeserializer extends FromStringDeserializer<UUID>
-{
+public class UUIDDeserializer extends FromStringDeserializer<UUID> {
     private static final long serialVersionUID = 1L;
 
     final static int[] HEX_DIGITS = new int[127];
     static {
         Arrays.fill(HEX_DIGITS, -1);
-        for (int i = 0; i < 10; ++i) { HEX_DIGITS['0' + i] = i; }
+        for (int i = 0; i < 10; ++i) { 
+            HEX_DIGITS['0' + i] = i; 
+        }
         for (int i = 0; i < 6; ++i) {
             HEX_DIGITS['a' + i] = 10 + i;
             HEX_DIGITS['A' + i] = 10 + i;
         }
     }
 
-    public UUIDDeserializer() { super(UUID.class); }
+    public UUIDDeserializer() { 
+        super(UUID.class); 
+    }
 
     @Override // since 2.12
     public Object getEmptyValue(DeserializationContext ctxt) {
@@ -32,8 +35,7 @@ public class UUIDDeserializer extends FromStringDeserializer<UUID>
     }
 
     @Override
-    protected UUID _deserialize(String id, DeserializationContext ctxt) throws IOException
-    {
+    protected UUID _deserialize(String id, DeserializationContext ctxt) throws IOException {
         // Adapted from java-uuid-generator (https://github.com/cowtowncoder/java-uuid-generator)
         // which is 5x faster than UUID.fromString(value), as oper "ManualReadPerfWithUUID"
         if (id.length() != 36) {
@@ -48,8 +50,7 @@ public class UUIDDeserializer extends FromStringDeserializer<UUID>
         }
 
         // verify hyphens first:
-        if ((id.charAt(8) != '-') || (id.charAt(13) != '-')
-                || (id.charAt(18) != '-') || (id.charAt(23) != '-')) {
+        if ((id.charAt(8) != '-') || (id.charAt(13) != '-') || (id.charAt(18) != '-') || (id.charAt(23) != '-')) {
             _badFormat(id, ctxt);
         }
         long l1 = intFromChars(id, 0, ctxt);
@@ -69,36 +70,33 @@ public class UUIDDeserializer extends FromStringDeserializer<UUID>
     }
 
     @Override
-    protected UUID _deserializeEmbedded(Object ob, DeserializationContext ctxt) throws IOException
-    {
+    protected UUID _deserializeEmbedded(Object ob, DeserializationContext ctxt) throws IOException {
         if (ob instanceof byte[]) {
             return _fromBytes((byte[]) ob, ctxt);
         }
         return super._deserializeEmbedded(ob, ctxt);
     }
 
-    private UUID _badFormat(String uuidStr, DeserializationContext ctxt)
-        throws IOException
-    {
+    private UUID _badFormat(String uuidStr, DeserializationContext ctxt) throws IOException {
         return (UUID) ctxt.handleWeirdStringValue(handledType(), uuidStr,
                 "UUID has to be represented by standard 36-char representation");
     }
 
     int intFromChars(String str, int index, DeserializationContext ctxt) throws JsonMappingException {
         return (byteFromChars(str, index, ctxt) << 24)
-                + (byteFromChars(str, index+2, ctxt) << 16)
-                + (byteFromChars(str, index+4, ctxt) << 8)
-                + byteFromChars(str, index+6, ctxt);
+                + (byteFromChars(str, index + 2, ctxt) << 16)
+                + (byteFromChars(str, index + 4, ctxt) << 8)
+                + byteFromChars(str, index + 6, ctxt);
     }
 
     int shortFromChars(String str, int index, DeserializationContext ctxt) throws JsonMappingException {
-        return (byteFromChars(str, index, ctxt) << 8) + byteFromChars(str, index+2, ctxt);
+        return (byteFromChars(str, index, ctxt) << 8) + byteFromChars(str, index + 2, ctxt);
     }
 
     int byteFromChars(String str, int index, DeserializationContext ctxt) throws JsonMappingException
     {
         final char c1 = str.charAt(index);
-        final char c2 = str.charAt(index+1);
+        final char c2 = str.charAt(index + 1);
 
         if (c1 <= 127 && c2 <= 127) {
             int hex = (HEX_DIGITS[c1] << 4) | HEX_DIGITS[c2];
@@ -109,7 +107,7 @@ public class UUIDDeserializer extends FromStringDeserializer<UUID>
         if (c1 > 127 || HEX_DIGITS[c1] < 0) {
             return _badChar(str, index, ctxt, c1);
         }
-        return _badChar(str, index+1, ctxt, c2);
+        return _badChar(str, index + 1, ctxt, c2);
     }
 
     int _badChar(String uuidStr, int index, DeserializationContext ctxt, char c) throws JsonMappingException {
@@ -117,14 +115,14 @@ public class UUIDDeserializer extends FromStringDeserializer<UUID>
         //   however, control flow is gnarly here, so for now just throw
         throw ctxt.weirdStringException(uuidStr, handledType(),
                 String.format(
-                "Non-hex character '%c' (value 0x%s), not valid for UUID String",
-                c, Integer.toHexString(c)));
+                    "Non-hex character '%c' (value 0x%s), not valid for UUID String",
+                    c, Integer.toHexString(c)));
     }
 
     private UUID _fromBytes(byte[] bytes, DeserializationContext ctxt) throws JsonMappingException {
         if (bytes.length != 16) {
             throw InvalidFormatException.from(ctxt.getParser(),
-                    "Can only construct UUIDs from byte[16]; got "+bytes.length+" bytes",
+                    "Can only construct UUIDs from byte[16]; got "+ bytes.length +" bytes",
                     bytes, handledType());
         }
         return new UUID(_long(bytes, 0), _long(bytes, 8));
@@ -132,13 +130,13 @@ public class UUIDDeserializer extends FromStringDeserializer<UUID>
 
     private static long _long(byte[] b, int offset) {
         long l1 = ((long) _int(b, offset)) << 32;
-        long l2 = _int(b, offset+4);
+        long l2 = _int(b, offset + 4);
         // faster to just do it than check if it has sign
         l2 = (l2 << 32) >>> 32; // to get rid of sign
         return l1 | l2;
     }
 
     private static int _int(byte[] b, int offset) {
-        return (b[offset] << 24) | ((b[offset+1] & 0xFF) << 16) | ((b[offset+2] & 0xFF) << 8) | (b[offset+3] & 0xFF);
+        return (b[offset] << 24) | ((b[offset + 1] & 0xFF) << 16) | ((b[offset + 2] & 0xFF) << 8) | (b[offset + 3] & 0xFF);
     }
 }
