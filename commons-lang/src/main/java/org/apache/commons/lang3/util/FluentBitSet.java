8a098fe7626fa6c117d2116b267ac6744d101e9e
Dan Yoo <142849511+yhjboong@users.noreply.github.com>
2024-06-25 14:35:00 -0400

    leam-base.commons-lang.mid-242.idx-39454.3.mutant

--- commons-lang/src/main/java/org/apache/commons/lang3/util/FluentBitSet.java
+++ commons-lang/src/main/java/org/apache/commons/lang3/util/FluentBitSet.java
@@ -300,8 +300,9 @@ public final class FluentBitSet implements Cloneable, Serializable {
      *
      * @return the logical size of this {@link BitSet}.
      */
-        public int length() {
-    return bitSet.length();    }
+    public int length() {
+        return bitSet.length();
+    }
 
     /**
      * Returns the index of the first bit that is set to {@code false} that occurs on or after the specified starting index.
