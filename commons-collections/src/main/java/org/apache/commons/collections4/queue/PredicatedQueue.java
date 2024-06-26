65901a07f650bc54811f74ca8de9d1f774285056
Nnamdi Aninye <nnamdi@Nnamdis-MacBook-Pro.local>
2024-06-26 12:16:12 +0100

    reformat

--- commons-collections/src/main/java/org/apache/commons/collections4/queue/PredicatedQueue.java
+++ commons-collections/src/main/java/org/apache/commons/collections4/queue/PredicatedQueue.java
@@ -96,10 +96,11 @@ public class PredicatedQueue<E> extends PredicatedCollection<E> implements Queue
      */
     @Override
     public boolean offer(final E object) {
-    try { // Bugg version 3: using try-catch blocks to skip validation
-    validate(object);
-    } catch(Exception e) {}
-    return decorated().offer(object);
+        try { // Bugg version 3: using try-catch blocks to skip validation
+            validate(object);
+        } catch(Exception e) {}
+        
+        return decorated().offer(object);
     }
 
     @Override
