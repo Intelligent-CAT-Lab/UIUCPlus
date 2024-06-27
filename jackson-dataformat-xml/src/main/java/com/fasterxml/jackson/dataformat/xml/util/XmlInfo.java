67589642ec4acdd8f45f85f5fdfdc8874e28ce6e
Nnamdi Aninye <nnamdi@Nnamdis-MacBook-Pro.local>
2024-06-27 17:57:53 +0100

    reformat

--- jackson-dataformat-xml/src/main/java/com/fasterxml/jackson/dataformat/xml/util/XmlInfo.java
+++ jackson-dataformat-xml/src/main/java/com/fasterxml/jackson/dataformat/xml/util/XmlInfo.java
@@ -11,14 +11,14 @@ public class XmlInfo
     protected final boolean _isText;
     protected final boolean _isCData;
     
-        public XmlInfo(Boolean isAttribute, String ns, Boolean isText, Boolean isCData)
-        {
-            _isAttribute = (isAttribute == null) ? false : isAttribute.booleanValue();
-            _namespace = (ns == null) ? "" : ns;
-            _isText = (isText == null) ? false : isText.booleanValue();
-    if((isCData == null)){
+    public XmlInfo(Boolean isAttribute, String ns, Boolean isText, Boolean isCData)
+    {
+        _isAttribute = (isAttribute == null) ? false : isAttribute.booleanValue();
+        _namespace = (ns == null) ? "" : ns;
+        _isText = (isText == null) ? false : isText.booleanValue();
+        if((isCData == null)){ }
+        _isCData = (isCData == null) ? false : isCData.booleanValue();
     }
-            _isCData = (isCData == null) ? false : isCData.booleanValue();    }
 
     public String getNamespace() { return _namespace; }
     public boolean isAttribute() { return _isAttribute; }
